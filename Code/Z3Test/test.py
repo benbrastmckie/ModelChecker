ad536612e6d4eb3d3c901fddba8545c39eb0a9e2
benbrastmckie <benbrastmckie@gmail.com>
2024-02-07 21:34:41 -0500

    update

--- Code/Z3Test/test.py
+++ Code/Z3Test/test.py
@@ -2,6 +2,11 @@ from z3 import *
 
 Tie, Shirt = Bools("Tie Shirt")
 s = Solver()
-s.add(Or(Tie, Shirt), Or(Not(Tie), Shirt), Or(Not(Tie), Not(Shirt)))
+s.add(
+    Or(Tie, Shirt), 
+    Or(Not(Tie), Shirt), 
+    Or(Not(Tie), Not(Shirt))
+)
 print(s.check())
 print(s.model())
+
