69d1402bb39c604b6ff02b503d1a18655ce6a267
mfbuit46 <mfbuit46@mit.edu>
2024-02-14 16:05:21 -0500

    small changes

--- Code/Z3Test/test.py
+++ Code/Z3Test/test.py
@@ -5,3 +5,33 @@ s = Solver()
 s.add(Or(Tie, Shirt), Or(Not(Tie), Shirt), Or(Not(Tie), Not(Shirt)))
 print(s.check())
 print(s.model())
+
+p, q = Bools('p q')
+demorgan = And(p, q) == Not(Or(Not(p), Not(q)))
+print (demorgan)
+
+def prove(f):
+    s = Solver()
+    s.add(Not(f))
+    if s.check() == unsat:
+        print ("proved")
+    else:
+        print ("failed to prove")
+
+print ("Proving demorgan...")
+prove(demorgan)
+
+p, q = Bools('p q')
+demorgan = And(p, q) == Not(Or(Not(p), Not(q)))
+print (demorgan)
+
+def prove(f):
+    s = Solver()
+    s.add(Not(f))
+    if s.check() == unsat:
+        print ("proved")
+    else:
+        print ("failed to prove")
+
+print ("Proving demorgan...")
+prove(demorgan)
