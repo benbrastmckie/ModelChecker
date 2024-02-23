dd33a11bf64696c31e40d373e998b487f277c06b
mfbuit46 <mfbuit46@mit.edu>
2024-02-22 23:57:00 -0500

    cleaned things up based on 2/21 mtg

--- Code/Prefix_Infix/prefix_infix.py
+++ Code/Prefix_Infix/prefix_infix.py
@@ -1,6 +1,15 @@
 import doctest
 from z3 import *
+'''
+OVERVIEW
+All sentence letters are capital letters. All commands must be strictly in lowercase; they can have double backslash, a forward slash, or nothing (nothing so long as the first letter is lowercase)
+the unary operators are defined in a separate set for clarity in the code
+'''
 
+
+sentence_stuff = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','Z','W','Y','Z'}
+operator_stuff = {'\\','/','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+unary_operators = {'\\neg', '/neg', 'neg'}
 def tokenize(str_exp):
     """
     >>> tokenize("(A /wedge (B /vee C))")
@@ -14,12 +23,10 @@ def tokenize(str_exp):
 
     >>> tokenize('/neg A')
     ['/neg', 'A']
-
     """
     split_str = str_exp.split() # small issue here with doctest cases and backslashes
 
     def tokenize_improved_input(split_str):
-        sentence_stuff = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         if len(split_str) == 1:
             # split_str is a list with one elem (has been called recursively or is last elem)
             base_string = split_str[0]  # base_string is a string
@@ -31,11 +38,7 @@ def tokenize(str_exp):
                 tokenized_l = tokenize_improved_input([base_string[:-1]])
                 tokenized_l.append(")")
                 return tokenized_l
-            elif "\\" or "/" in base_string:  # latex operator case
-                return split_str
-            elif base_string in sentence_stuff: # sentence letter case
-                return split_str
-            raise ValueError(base_string) # these cases should be exhaustive
+            return split_str # else case: covers sentence letter case and latex operator case
         tokenized_l = tokenize_improved_input([split_str[0]])
         tokenized_l.extend(tokenize_improved_input(split_str[1:]))
         return tokenized_l
@@ -64,8 +67,6 @@ def main_op_index(tokenized_expression):
     Starting after the expression's initial parenthesis, the point
     at which the number of left parentheses equals the number of right is the
     first expression (as it is closed there)
-    # NOTE: what is the first expression?
-    # consider: ((A \\op (B \\op C)) \\op (D \\op E))
     ASSUMES FIRST CHAR IS LEFT PARENTH. IF NOT CASE, EQN PROLLY SHOULDN'T BE HERE
     >>> main_op_index(tokenize('(A /wedge (B /vee C))'))
     2
@@ -78,52 +79,54 @@ def main_op_index(tokenized_expression):
 
     >>> main_op_index(tokenize('((/neg A /vee B) /wedge C)'))
     7
+
+    >>> main_op_index(tokenize('((A \\op (B \\op C)) \\op (D \\op E))'))
+    10
     """
     left_parentheses = 0
     right_parentheses = 0
     if tokenized_expression[0] != '(':
         raise ValueError(tokenized_expression, 'this case probably shouldnt be being raised by this function')
-    for i, token in enumerate(tokenized_expression[1:]): # [1:] to exclude the complexity of the matrix operator
+    for i, token in enumerate(tokenized_expression[1:]): # [1:] to exclude the left parenth (thus complexity) of the main operator
         if token == "(":
             left_parentheses += 1
-        # elif '\\' in seq and not left_parentheses:
-        #     return left_parentheses
         elif token == ")":
             right_parentheses += 1
-        elif 'neg' in token:
+        elif token in unary_operators: # ignore this case since we care about binary complexity
             continue
         if left_parentheses == right_parentheses:
-            return i + 2 # +1 bc list is [1:], and +1 bc it's next elem where the matrix op is
+            return i + 2 # +1 bc list is [1:] and we want original index, and +1 bc it's next elem where the main op is
 
 
 def parse(tokens):
     """
     >>> parse(tokenize("(A /wedge (B /lor C))"))
-    ['/wedge', 'A', ['/lor', 'B', 'C']]
+    ['/wedge', ['A'], ['/lor', ['B'], ['C']]]
 
     >>> parse(tokenize("/neg A"))
-    ['/neg', 'A']
+    ['/neg', ['A']]
     
-    >>> parse(tokenize("A")) # note: atomic sentence should return a string
-    'A'
+    >>> parse(tokenize("A")) # note: atomic sentence should return a single element list
+    ['A']
+
+    >>> parse(tokenize('((A /op (B /op C)) /op (D /op E))'))
+    ['/op', ['/op', ['A'], ['/op', ['B'], ['C']]], ['/op', ['D'], ['E']]]
     """
     comp_tokens = binary_comp(tokens)
-    if 'neg' in tokens[0]:
+    if tokens[0] in unary_operators:
         return [tokens[0], parse(tokens[1:])]
     if comp_tokens == 0:
         token = tokens[0]
-        return token
-    matrix_index = main_op_index(tokens)
-    op_str = tokens[matrix_index]  # determines how far the operation is
-    left_expression = tokens[1 : matrix_index]  # from 1 (exclude first parenthesis) to the same
-    # pos of above, bc its exclusive
-    right_expression = tokens[matrix_index + 1 : -1]  # from pos of op plus 1 to the penultimate,
-    # thus excluding the last parentheses, which belongs to the matrix expression
+        return [token]
+    main_operator_index = main_op_index(tokens)
+    op_str = tokens[main_operator_index]  # determines how far the operation is
+    left_expression = tokens[1 : main_operator_index]  # start 1 (exclude first parenthesis), stop at same pos of above (exclusive)
+    right_expression = tokens[main_operator_index + 1 : -1]  # from pos of op plus 1 to the penultimate, thus excluding the last parentheses, which belongs to the main expression
     return [op_str, parse(left_expression), parse(right_expression)]
 
 
 def Prefix(A):
-    """takes a sentence in Infix notation and translates it to prefix notation"""
+    """takes a sentence in Infix notation and translates it to Prefix notation"""
     return parse(tokenize(A))
 
 
@@ -133,4 +136,7 @@ def Infix(A):
 
 
 doctest.testmod()
-print(tokenize("(A \\wedge (B \\vee C))")) # the doctests fail, but this works. Need to do double backslash for abfnrtv.
+# print(tokenize("(A \\wedge (B \\vee C))")) # the doctests fail, but this works. Need to do double backslash for abfnrtv.
+print(Prefix("(A \\wedge (B \\vee \\neg C))"))
+print(Prefix("A"))
+print(Prefix('((A \\op (B \\op C)) \\op (D \\op E))'))
