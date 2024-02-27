f8b0ed111a30594d14fbd0df4c601718c3a2d661
mfbuit46 <mfbuit46@mit.edu>
2024-02-27 14:36:15 -0500

    new prefix_infix func and some parts stuff

--- Code/Z3Test/parts_part2.py
+++ Code/Z3Test/parts_part2.py
@@ -33,20 +33,18 @@ y = BitVec("y", 7)
 z = BitVec("z", 7)
 w = BitVec("w", 7)
 
-solver.add( # NOTE: right now it is not finding a model but should be able to
+solver.add(
     ForAll([x, y], Implies(
         And(is_part_of(x, y), possible(y)),
         possible(x))
     ),
-    Not(possible(x)),
+    Not(possible(y)),
     is_part_of(x, y),
-        # possible(y),
-        # NOTE: once a model is found, uncomment the above to see if this makes it unsat
-    possible(z) == True,
+    possible(z),
     is_part_of(w,z),
     z == 5, # change this to 0 and no model is found. This is good and checks out with intuitive result
     w != 0,
-    x != y
+    x != y,
 )
 
 # Check if there exists a model
@@ -69,67 +67,66 @@ else:
     print("No model found")
 
 # below is the output (the monster). Seems that this defaults to nand format :(
-output = And(
-        Not(
-            And(
-                Not(Var(0) == 1),
-                Not(Var(0) == 5),
-                Not(Var(0) == 8),
-                Not(Var(0) == 52),
-                Not(Var(0) == 40),
-                Not(Var(0) == 0),
-                Not(Var(0) == 4),
-                Not(Var(0) == 54),
-                Not(Var(0) == 47),
-                Not(Var(0) == 116)
-            )
-        ),
-        Not(
-            And(
-                Var(0) == 54,
-                Not(Var(0) == 47),
-                Not(Var(0) == 116)
-            )
-        ),
-        Not(
-            And(
-                Var(0) == 40,
-                Not(Var(0) == 0),
-                Not(Var(0) == 4),
-                Not(Var(0) == 54),
-                Not(Var(0) == 47),
-                Not(Var(0) == 116)
-            )
-        ),
-        Not(Var(0) == 116),
-        Not(
-            And(
-                Var(0) == 52,
-                Not(Var(0) == 40),
-                Not(Var(0) == 0),
-                Not(Var(0) == 4),
-                Not(Var(0) == 54),
-                Not(Var(0) == 47),
-                Not(Var(0) == 116)
-            )
-        ),
-        Not(
-            And(
-                Var(0) == 47, 
-                Not(Var(0) == 116)
-            )
-        ),
-        Not(
-            And(
-                Var(0) == 8,
-                Not(Var(0) == 52),
-                Not(Var(0) == 40),
-                Not(Var(0) == 0),
-                Not(Var(0) == 4),
-                Not(Var(0) == 54),
-                Not(Var(0) == 47),
-                Not(Var(0) == 116)
-            )
-        )
-    )
-simplify(a)
+#output = And(
+#        Not(
+#            And(
+#                Not(Var(0) == 1),
+#                Not(Var(0) == 5),
+#                Not(Var(0) == 8),
+#                Not(Var(0) == 52),
+#                Not(Var(0) == 40),
+#                Not(Var(0) == 0),
+#                Not(Var(0) == 4),
+#                Not(Var(0) == 54),
+#                Not(Var(0) == 47),
+#                Not(Var(0) == 116)
+#            )
+#        ),
+#        Not(
+#            And(
+#                Var(0) == 54,
+#                Not(Var(0) == 47),
+#                Not(Var(0) == 116)
+#            )
+#        ),
+#        Not(
+#            And(
+#                Var(0) == 40,
+#                Not(Var(0) == 0),
+#                Not(Var(0) == 4),
+#                Not(Var(0) == 54),
+#                Not(Var(0) == 47),
+#                Not(Var(0) == 116)
+#            )
+#        ),
+#        Not(Var(0) == 116),
+#        Not(
+#            And(
+#                Var(0) == 52,
+#                Not(Var(0) == 40),
+#                Not(Var(0) == 0),
+#                Not(Var(0) == 4),
+#                Not(Var(0) == 54),
+#                Not(Var(0) == 47),
+#                Not(Var(0) == 116)
+#            )
+#        ),
+#        Not(
+#            And(
+#                Var(0) == 47, 
+#                Not(Var(0) == 116)
+#            )
+#        ),
+#        Not(
+#            And(
+#                Var(0) == 8,
+#                Not(Var(0) == 52),
+#                Not(Var(0) == 40),
+#                Not(Var(0) == 0),
+#                Not(Var(0) == 4),
+#                Not(Var(0) == 54),
+#                Not(Var(0) == 47),
+#                Not(Var(0) == 116)
+#            )
+#        )
+#    )
