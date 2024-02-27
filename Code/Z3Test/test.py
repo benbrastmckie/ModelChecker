8fc33a4ad324acc8a7117c9643bed531165d3c5c
mfbuit46 <mfbuit46@mit.edu>
2024-02-27 15:11:08 -0500

    added a total_fusion function

--- Code/Z3Test/test.py
+++ Code/Z3Test/test.py
@@ -78,19 +78,11 @@ def is_atomic(bit_vector):
         And(bit_vector != 0, 0 == (bit_vector & (bit_vector - 1)))
     )  # this is taken from a Z3 documentation place thing
 
-
-# for i in range(500): # this short for loop tests is_atomic. Uncomment it to use it; it works.
-#     bitvec = BitVecVal(i,9)
-#     if is_atomic(bitvec):
-#         print(bitvec.sexpr())
-
-
 def fusion(bit_s, bit_t):
     fused = bit_s | bit_t  # | is the or operator
     return simplify(
         fused
     )  # this turns it from bvor to #b. The or operator | seems to return an "or" object of sorts, so simplify turns it into a bitvector object.
-
 
 def is_part_of(bit_s, bit_t):
     return (
@@ -98,6 +90,25 @@ def is_part_of(bit_s, bit_t):
     )  # testing if fusion equals bit_t, as definition does
     # adding the sexpr()s above seemed to do the trick, not sure why.
 
+def total_fusion(list_of_states):
+    '''returns the fusion of a list of states'''
+    fusion_of_first_two = fusion(list_of_states[0], list_of_states[1])
+    if len(list_of_states) == 2: # base case: fuse 2
+        return fusion_of_first_two
+    # recursive step: fuse first two and run the func on the next
+    new_list_of_states = [fusion_of_first_two]
+    new_list_of_states.extend(list_of_states[2:])
+    return total_fusion(new_list_of_states)
+
+#short program to test total_fusion
+atomic_states = []
+for i in range(500): # this short for loop tests is_atomic. Uncomment it to use it; it works.
+    bitvec = BitVecVal(i,9)
+    if is_atomic(bitvec):
+        print(bitvec.sexpr())
+        atomic_states.append(bitvec)
+print(atomic_states)
+print(total_fusion(atomic_states).sexpr()) # prints #b111111111—so total_fusion works!
 
 # we can set a bitvector equal to a number with BitVecVal(value, bits).
 # THIS type of bitvector can be represented as a vector with self.sexpr()
