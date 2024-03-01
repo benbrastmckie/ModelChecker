67d881ea46aaec6d156eefb98138103259021b56
benbrastmckie <benbrastmckie@gmail.com>
2024-02-29 22:29:45 -0500

    built concrete model but ran into bugs; updated TODOs

--- Code/definitions.py
+++ Code/definitions.py
@@ -3,7 +3,7 @@
 from z3 import (
     # simplify,
     # Not,
-    # Exists,
+    Exists,
     ForAll,
     Implies,
     BoolSort,
@@ -13,28 +13,39 @@ from z3 import (
     Consts,
     Function,
     And,
-    Var,
+    # Var,
 )
+
 
 ### INFO ###
 
 # this file will have all declarations and definitions needed to build models
 
+
 ### DECLARATIONS ###
 
 # number of atomic states
-N = 7
+N = 3
 
 # sentence letters: sort definition, constants, and variables
 # B: do we need a constant for each sentence letter in sentence_letters?
 AtomSort = DeclareSort("AtomSort")
 A, B = Consts("A B", AtomSort)
 
-# B: I'm not sure these will do what we want but included them to experiment
-X = Var("X", AtomSort)
-Y = Var("Y", AtomSort)
+# B: Seems like the following could be useful to quantify over propositions
+# B: Couldn't get this to work
+# X = Var("X", AtomSort)
+# Y = Var("Y", AtomSort)
 
 # declare bitvector variables used for states
+a = BitVec("a", N)
+b = BitVec("b", N)
+c = BitVec("c", N)
+
+r = BitVec("r", N)
+s = BitVec("s", N)
+t = BitVec("t", N)
+
 x = BitVec("x", N)
 y = BitVec("y", N)
 z = BitVec("z", N)
@@ -49,60 +60,69 @@ possible = Function("possible", BitVecSort(N), BoolSort())
 verify = Function("verify", BitVecSort(N), AtomSort, BoolSort())
 falsify = Function("falsify", BitVecSort(N), AtomSort, BoolSort())
 
+
 ### DEFINITIONS ###
 
 
 def is_atomic(bit_vector):
     return And(bit_vector != 0, 0 == (bit_vector & (bit_vector - 1)))
-    # this is taken from section 3.4 of https://z3prover.github.io/papers/programmingz3.html#sec-sorts
-    # TODO: move relevant notes here to gossary
 
 
 def fusion(bit_s, bit_t):
     return bit_s | bit_t
-    # fused = bit_s | bit_t  # | is the or operator
-    # return simplify(fused)
-    # this turns it from bvor to #b. The or operator | seems to return an "or" object of sorts, so simplify turns it into a bitvector object.
-    # TODO: move relevant notes here to gossary
 
 
 def is_part_of(bit_s, bit_t):
     return fusion(bit_s, bit_t) == bit_t
-    # return fusion(bit_s, bit_t).sexpr() == bit_t.sexpr()
-    # testing if fusion equals bit_t, as definition does
-    # adding the sexpr()s above seemed to do the trick, not sure why.
-    # TODO: move relevant notes here to gossary
 
+
+def compatible(bit_x, bit_y):
+    return possible(fusion(bit_x, bit_y))
     # TODO: compatible def
 
-    # TODO: maximality def
-    # NOTE: it might be OK to define the sort for a variable x here
-    # that variable could then be used in the def of maximal
 
-    # TODO: world def in terms of compatible and maximal defs
-
-    # TODO: compatible_part def
-
-    # TODO: alternative def
-
-    # TODO: extended_verify and extended_falsify functions
-
-    # TODO: true def
+def maximal(bit_w):
+    return ForAll(
+        x,
+        Implies(
+            And(possible(x), possible(fusion(x, bit_w))),
+            is_part_of(x, bit_w),
+        ),
+    )
 
 
-# B: check that quantification over x is working as intended
-# TODO: rewrite in terms of possible and maximal defs
 def is_world(bit_w):
     return And(
         possible(bit_w),
+        maximal(bit_w),
+    )
+
+
+def compatible_part(bit_x, bit_w, bit_y):
+    return And(
+        is_part_of(bit_x, bit_w),
+        compatible(bit_x, bit_y),
         ForAll(
-            x,
+            z,
             Implies(
-                And(possible(x), possible(fusion(x, bit_w))),
-                is_part_of(x, bit_w),
+                And(is_part_of(z, bit_w), compatible(z, bit_y), is_part_of(bit_x, z)),
+                x == z,
             ),
         ),
     )
+
+
+def alternative(bit_w, bit_y, bit_u):
+    return And(
+        is_world(bit_u),
+        is_part_of(bit_y, bit_u),
+        Exists(z, And(is_part_of(z, bit_u), compatible_part(z, bit_w, bit_y))),
+    )
+
+
+    # TODO: extended_verify and extended_falsify functions (see Strategies)
+
+    # TODO: true def (see Strategies)
 
 
 def total_fusion(list_of_states):
