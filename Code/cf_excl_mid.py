11eb1a52bd964e1370de1feb5b8bd0c29d1fb04c
benbrastmckie <benbrastmckie@gmail.com>
2024-03-07 22:06:02 -0500

    added simplification of disjunctive antecedent file

--- Code/cf_excl_mid.py
+++ Code/cf_excl_mid.py
@@ -37,6 +37,7 @@ from definitions import (
     z,
     A,
     B,
+    C,
     X,
     # Y,
     fusion,
@@ -54,10 +55,8 @@ from definitions import (
     bitvec_to_substates,
     maximal,
     Equivalent,
-    # parthood
 )
 
-# dummy = BitVec('dummy',N)
 solver = Solver()
 
 solver.add(
@@ -66,19 +65,17 @@ solver.add(
     # every part of a possible state is possible
     ForAll([x, y], Exists(z, fusion(x, y) == z)),
     # states are closed under fusion
-    ForAll(w, Equivalent(
-        world(w),
-        # is_world(w),  # NOTE: ditto to NOTE for is_alternative below
-        And(possible(w), maximal(w))
-    )),
+    ForAll(
+        w,
+        Equivalent(
+            world(w),
+            And(possible(w), maximal(w)),
+        ),
+    ),
     ForAll(
         [u, y],
         Equivalent(  # TODO: replace with Z3 equiv if any?
             alternative(u, y, w),
-            # is_alternative(u, y, w),
-            # NOTE: making constraints on alternative() explicit here could
-            # make things easier to tweak and keep track of rather than having
-            # two similar sounding terms, one defined and one primitive
             And(
                 world(u),
                 is_part_of(y, u),
@@ -86,41 +83,35 @@ solver.add(
             ),
         ),
     ),
-
     # MODEL CONSTRAINT
     ForAll(X, proposition(X)),  # every X of AtomSort is a proposition
-
     # EVAL CONSTRAINTS
     world(w),  # there is a world w
-    is_part_of(s, w),  # s is a part of w
-    verify(s, A),  # s verifies A
-    is_part_of(t, w),  # t is part of w
-    verify(t, B),  # t verifies B
+    ForAll(  # A \vee B \boxright C
+        [s, v],
+        Implies(
+            And(
+                Or(
+                    verify(s, A),
+                    verify(s, B),
+                    Exists(
+                        [a, b],
+                        And(
+                            s == fusion(a, b),
+                            verify(a, A),
+                            verify(b, B),
+                        ),
+                    ),
+                ),
+                is_alternative(v, s, w),
+            ),
+            Exists(t, And(is_part_of(t, v), verify(t, C))),
+        ),
+    ),
 
-    # CF constraints: it is not true that, if A were the case, B would be the case
-    verify(y, A),
-    is_alternative(u, y, w),
-    Exists(b, And(is_part_of(b, u), falsify(b, B))),
-
-    # NOTE: although the above is equivalent to the below modulo exhaustivity
-    # the latter produces truth-value gaps (see issue on github)
-
-    # ForAll(b, Implies(is_part_of(b, u), Not(verify(b, B)))),
-
-        # NOTE: replacing the CF constraints above with the below produces
-        # models with no A-alternatives to w despite what the constraint would
-        # seem to require. However, adding the constraint 'falsify(z, B)'
-        # populates A-alternatives. I'm not sure why this is.
-    # Not(  # in w, it is not the case that if A were true then B would be true
-    #     ForAll(
-    #         [a, v],
-    #         Implies(
-    #             And(verify(a, A), is_alternative(v, a, w)),
-    #             Exists(b, And(is_part_of(b, v), verify(b, B))),
-    #         ),
-    #     ),
-    # ),
-
+    verify(r, A),
+    is_alternative(v, r, w),
+    Exists(t, And(is_part_of(t, v), falsify(t, C))),
 )
 
 if solver.check() == sat:
@@ -128,14 +119,14 @@ if solver.check() == sat:
     model = solver.model()
 
     # TODO: eventually replace with something more general
-    sentence_letter_objects = [A, B]
+    sentence_letter_objects = [A, B, C]
     sentence_letter_names = {
         S.sexpr() for S in sentence_letter_objects
     }  # set because we're only testing for membership
     # M: got the for loop issue working. It was a type mismatch issue.
     # needed to make a list of sentence letter objects and names
 
-    # QUESTION: the above seems motivated by the role sentence_letter_names
+    # B: (QUESTION) the above seems motivated by the role sentence_letter_names
     # plays in all_states below. I wonder if there is a better way to define
     # all_states that does not need sentence_letter_names? perhaps we can
     # filter directly by asking if d in model.decls() is a bitvec?
@@ -151,11 +142,8 @@ if solver.check() == sat:
 
     print("States:")  # Print states
     for val in range(max_num * 2):
-        # bc binary; the best-case last one (stopped at) is the first one repeated, so we're good
-        # B: that makes good sense!
         test_state = BitVecVal(val, N)
         as_substates = bitvec_to_substates(test_state)
-        # print(f"TEST STATE: {test_state}")
         if as_substates in already_seen:
             break
         elif model.evaluate(world(test_state)):
@@ -191,7 +179,7 @@ if solver.check() == sat:
         #     if model.evaluate(parthood(model[state], model[w]))
         # }
 
-        # TODO: the aim here is to define a set of verifiers for S that are part
+        # TODO: the aim above is to define a set of verifiers for S that are part
         # of the designated world w so as to say whether S is true at w
         # there may be a better way to do this
 
@@ -210,3 +198,5 @@ if solver.check() == sat:
             print(f"{S}-alternatives to {bitvec_to_substates(model[w])} = ∅")
 
         # TODO: I couldn't figure out how to remove the quotes from the states
+else:
+    print("There are no models")
