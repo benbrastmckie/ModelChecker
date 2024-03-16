6e3c5ae65c81385b8a32bdd02dd8f3552564033b
mfbuit46 <mfbuit46@mit.edu>
2024-03-16 16:54:40 -0400

    worked on new relations

--- Code/ent_2.py
+++ Code/ent_2.py
@@ -8,15 +8,49 @@ sentence_letters = [A, B, C]
 
 solver = Solver()
 
+solver.add(
+    # FRAME CONSTRAINTS:
+    ForAll([x, y], Implies(And(possible(y), is_part_of(x, y)), possible(x))),
+    # every part of a possible state is possible
+    ForAll([x, y], Exists(z, fusion(x, y) == z)),
+    # states are closed under fusion
 
+    # MODEL CONSTRAINT
+    ForAll(X, proposition(X)),
+    # every X of AtomSort is a proposition
+    is_world(w),
+    # there is a world w
 
+    ForAll(
+        [a, v],
+        Implies(
+            And(verify(a, A), is_alternative(v, a, w)),
+            Exists(t, And(
+                is_part_of(t, v),
+                verify(t, B),
+            )),
+        ),
+    ),
+    # A => B is true in w
 
+    ForAll(
+        [b, v],
+        Implies(
+            And(verify(b, B), is_alternative(u, b, w)),
+            Exists(t, And(
+                is_part_of(t, u),
+                verify(t, C),
+            )),
+        ),
+    ),
+    # B => C is true in w
 
-
-
-
-
-
+    non_null_verify(c,A),
+    is_alternative(r,c,w),
+    is_part_of(s,r),
+    falsify(s,C),
+    # ~(A => C) is true in w 
+)
 
 if solver.check() == sat:
     print("\nThere is a model of:")
