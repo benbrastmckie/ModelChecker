7695e3f196050ca4e53e7c444d60dbfeba6a9749
mfbuit46 <mfbuit46@mit.edu>
2024-03-22 10:28:01 -0400

    trying to fix something

--- Code/prefix_infix.py
+++ Code/prefix_infix.py
@@ -165,8 +165,4 @@ def all_sentence_letters(input_sentences):
 sentences = [Prefix("(A \\wedge (B \\vee \\neg C))"), Prefix("A"), Prefix('((A \\op (B \\op Z)) \\op (D \\op E))')]
 # print(all_sentence_letters(sentences)) # correctly prints ['A', 'B', 'C', 'D', 'E', 'Z']
 # print(Prefix("\\neg (A \\boxright B)"))
-<<<<<<< HEAD
 # print(all_sentence_letters([Prefix("\\neg (A \\boxright B)")]))
-=======
-print(all_sentence_letters([Prefix("\\neg (A \\boxright B)")]))
->>>>>>> 8e52390 (new branch for trying everything together)
