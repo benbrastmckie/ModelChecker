0786be6fbabc431a21587ab03b0764329bcf4643
mfbuit46 <mfbuit46@mit.edu>
2024-04-02 17:24:43 -0400

    small changes

--- Code/test_complete.py
+++ Code/test_complete.py
@@ -4,12 +4,15 @@ from semantics import *
 from definitions import *
 from print import *
 # everything different is in prefix_infix and semantics. Everything else is the same.
-input_sentences = ['(A \\boxright B)','((A \\wedge B) \\boxright C)','\\neg (A \\boxright C)']
+# input_sentences = ['(A \\boxright B)','((A \\wedge B) \\boxright C)','\\neg (A \\boxright C)']
+# input_sentences = ['A','B','\\neg (A \\wedge B)']
+input_sentences = ['A','\\neg A'] # finds a model for a contradiction, something seems to be wrong
+# input_sentences = ['A','B','(A \\wedge B)']
 prefix_sentences = [Prefix(input_sent) for input_sent in input_sentences] # this works
 sentence_letters = all_sentence_letters(prefix_sentences) # this works
 
 solver = Solver()
-add_general_constraints(solver) # I should be working, it's nothing complicated
+add_general_constraints(solver) # should be working, it's nothing complicated
 add_input_constraints(solver, prefix_sentences) # I think this is where the problem's at
 # an issue may be whether the sentence letters are actually in the constraints. I think they are
 # but the issue may also be about states (ie, bitvecs) and quantifier binding. 
