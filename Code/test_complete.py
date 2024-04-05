87b7a600cde577d2f446c54a0be33878d350460a
benbrastmckie <benbrastmckie@gmail.com>
2024-04-05 16:08:26 -0400

    finished testing trace tooling

--- Code/test_complete.py
+++ Code/test_complete.py
@@ -2,7 +2,9 @@
 converts infix_sentences to Z3 constraints and adds to solver, printing results
 '''
 import time
-# import multiprocessing
+import cProfile
+# from pyinstrument import Profiler
+# import multiprocessing # NOTE: couldn't get this to work (see below)
 from semantics import (
     find_all_constraints,
     solve_constraints,
@@ -52,54 +54,61 @@ input_sentences = ['(A \\boxright C)','\\neg ((A \\wedge B) \\boxright C)'] # WE
 # input_sentences = ['(A \\boxright B)','\\neg B','\\neg (\\neg B \\boxright \\neg A)']
 # input_sentences = ['\\neg A','\\neg (A \\boxright B)','\\neg (A \\boxright \\neg B)']
 
-
-
 ################################
 ############ SOLVER ############
 ################################
 
 
+"""find constraints and sentence letters"""
 constraints, sentence_letters = find_all_constraints(input_sentences)
 
+"""find model in any in timed enviornment"""
 start_time = time.time() # start benchmark timer
+# cProfile.run('model = solve_constraints(constraints)') # for detailed report
 model = solve_constraints(constraints)
 end_time = time.time()
 
+"""print results"""
 print_model(model, input_sentences, sentence_letters)
 execution_time = end_time - start_time
 print(f"Execution time: {execution_time}\n")
 
 
-# NOTE: external tool for studying execution flow
-# import pyinstrument
-#
-# profiler = pyinstrument.Profiler()
+# NOTE: this works; must import pyinstrument
+# profiler = Profiler()
 # profiler.start()
-# your_algorithm()
+# CODE
 # profiler.stop()
-# print(profiler.output_text())
+# print(profiler.output_text(unicode=True, color=True))
 
 # NOTE: adapt below to run multiprocessing
 # if __name__ == '__main__':
-#     # Create a multiprocessing pool
-#     constraints = find_constraints(input_sentences)
+#     constraints, sentence_letters = find_all_constraints(input_sentences)
+#
+#     # Divide constraints into smaller subsets
 #     num_processes = multiprocessing.cpu_count() - 2  # Use all but two of the number of CPU cores
+#     chunk_size = len(constraints) // num_processes
+#     chunks = [constraints[i:i+chunk_size] for i in range(0, len(constraints), chunk_size)]
+#
+#     # Create a multiprocessing pool
 #     with multiprocessing.Pool(processes=num_processes) as pool:
-#         results = pool.map(solve_constraints, [constraints] * num_processes)
-#         # Run the solver concurrently for each set of constraints
+#         # Distribute solving tasks to different processes
+#         results = pool.map(solve_con, chunks)
+#
+#     # Combine results from different processes
+#     combined_results = combine_results(results)
 #
 #     # Close the pool to free up resources
 #     pool.close()
 #     pool.join()
 #
 #     # Process the results
-#     for result in results:
-#         if result is not None:
-#             print("There is a model of:")
-#             print(input_sentences)
-#             print_states(model)
-#             print_evaluation(model, sentence_letters)
-#             print_propositions(model, sentence_letters)
-#         else:
-#             print("\nThere are no models of:\n")
-#             print(input_sentences)
+#     if combined_results:
+#         print("There is a model of:")
+#         print(input_sentences)
+#         print_states(model)
+#         print_evaluation(model, sentence_letters)
+#         print_propositions(model, sentence_letters)
+#     else:
+#         print("\nThere are no models of:\n")
+#         print(input_sentences)
