e86b9ae2da3d5c860374a7db57848d075d4cfe22
mfbuit46 <mfbuit46@mit.edu>
2024-04-11 12:47:14 -0400

    worked on data structure

---
+++ Code/Z3Test/crimson.py
@@ -0,0 +1,136 @@
+# AIM: provide a concrete model that can be used to abstract from to build model generator functions
+from z3 import (
+    BitVecVal,
+    sat,
+    Exists,
+    ForAll,
+    Implies,
+    Solver,
+    And,
+    Not,
+    Or,
+)
+
+from definitions import (
+    N,
+    a,
+    b,
+    c,
+    is_proper_part_of,
+    non_null_falsify,
+    non_null_verify,
+    r,
+    s,
+    t,
+    u,
+    v,
+    w,
+    x,
+    y,
+    z,
+    A,
+    B,
+    X,
+    fusion,
+    is_part_of,
+    is_world,
+    possible,
+    non_null_verify,
+    verify,
+    falsify,
+    is_alternative,
+    proposition,
+)
+
+from print import print_evaluation, print_propositions, print_states
+
+# TODO: eventually replace sentence_letters with something more general
+sentence_letters = [A, B]
+
+solver = Solver()
+
+solver.add(
+
+    # FRAME CONSTRAINTS:
+    ForAll([x, y], Implies(And(possible(y), is_part_of(x, y)), possible(x))),
+    # every part of a possible state is possible
+    ForAll([x, y], Exists(z, fusion(x, y) == z)),
+    # states are closed under fusion
+
+    # MODEL CONSTRAINT
+    ForAll(X, proposition(X)),  # every X of AtomSort is a proposition
+
+    # EVAL CONSTRAINTS
+    is_world(w),
+    # there is a world w
+
+    # A is true in w
+    is_part_of(a, w),
+    verify(a, A),
+
+    # B is true in w
+    is_part_of(b, w),
+    verify(b, B),
+
+    # ~(A => B) is true in w
+    verify(c, A),
+    is_alternative(u, c, w),
+    is_part_of(s, u),
+    falsify(s, B),
+
+    # NOTE: although the above is equivalent to the below modulo exhaustivity
+    # the latter produces truth-value gaps (see issue on github)
+
+
+        # NOTE: replacing the CF constraints above with the below produces
+        # models with no A-alternatives to w despite what the constraint would
+        # seem to require. However, adding the constraint 'falsify(z, B)'
+        # populates A-alternatives. I'm not sure why this is.
+
+    # Exists(
+    #     [a, v],
+    #     And(verify(a, A), is_alternative(v, a, w),
+    #         ForAll(b, Or(
+    #             Not(is_part_of(b, v)),
+    #             Not(verify(b, B))
+    #             ),
+    #         ),
+    #     )
+    # ),
+
+    # Exists(
+    #     [a, v],
+    #     And(verify(a, A), is_alternative(v, a, w),
+    #         Exists(b, Implies(
+    #             is_part_of(b, v),
+    #             falsify(b, B)
+    #         )),
+    #     )
+    # ),
+
+    # Not(  # in w, it is not the case that if A were true then B would be true
+    #     ForAll(
+    #         [a, v],
+    #         Implies(
+    #             And(verify(a, A), is_alternative(v, a, w)),
+    #             Exists(b, And(is_part_of(b, v), verify(b, B))),
+    #         ),
+    #     ),
+    # ),
+
+)
+
+# print(type(bit_fusion(BitVecVal(5,5),BitVecVal(2,5))))
+# print(bit_fusion(BitVecVal(5,5),BitVecVal(2,5)).sort())
+
+
+if solver.check() == sat:
+    model = solver.model()
+    print("\nThere is a model of:")
+    print("A, B, ~(A => B)")
+    print_states(model)
+    print_evaluation(model, sentence_letters)
+    print_propositions(model, sentence_letters)
+else:
+    print("\nThere are no models of:")
+    print("A, B, A => B\n")
---
+++ Code/Z3Test/parts.py
@@ -0,0 +1,95 @@
+# from typing import TYPE_CHECKING
+# from z3 import *
+#
+# if TYPE_CHECKING:
+
+from z3 import (
+    BoolSort,
+    BitVecSort,
+    Function,
+    simplify,
+    Solver,
+    BitVec,
+    Not,
+    Exists,
+    And,
+    Implies,
+    ForAll,
+    sat,
+)
+
+# Define the predicates
+possible = Function("possible", BitVecSort(7), BoolSort())
+# dummy_predicate = Function("dummy")
+
+
+def fusion(bit_s, bit_t):
+    return bit_s | bit_t  # | is the or operator
+    # seems like the issue was that BitVecs work different from BitVecVals.
+    # I think we want to work with BitVecs? so rn it's good
+
+
+def is_part_of(bit_s, bit_t):
+    return fusion(bit_s, bit_t) == bit_t
+
+# Create an instance of Z3 solver
+solver = Solver()
+
+# Define the constraints
+x = BitVec("x", 7)
+y = BitVec("y", 7)
+z = BitVec("z", 7)
+w = BitVec("w", 7)
+
+solver.add(
+    ForAll([x, y], Implies(
+        And(is_part_of(x, y), possible(y)),
+        possible(x))
+    ),
+    Exists([x,y],And(
+        Not(possible(x)),
+        is_part_of(x, y),
+        # possible(y),
+        # NOTE: once a model is found, uncomment the above to see if this makes it unsat
+        # M: yeah it does
+           # B: That's great!
+           # B: This should allow to start setting up some of the other constraints
+           # B: Once we can use these to find countermodels by hand we can think about
+           # B: the functions that will generate these constraints (once it is working)
+    ))
+    # M: I don't think we actually ever add any of our vars to the solver bc x and y above are bound by quantifiers. 
+        # B: I tried commenting out the variables, but then it throws errors
+        # B: Perhaps defining the variable is more like defining its sort/type?
+        # B: I'll add this to `Questions`
+    # BONEYARD:
+    # Exists(x, possible(x)),
+    # Exists(y, Not(possible(y))),
+    # Exists([x,y],And(
+    #     possible(x),
+    #     possible(y),
+    #     Not(possible(fusion(x,y)))
+    # )),
+    # Exists(x,And(
+    #     possible(x),
+    #     ForAll(y, Implies(
+    #         possible(fusion(y,x)),
+    #         is_part_of(y,x)))
+    # )),
+    # Exists([x,y], And(possible(x), possible(y))),
+    # Exists([x,y], And(is_part_of(x,y),possible(y)))
+    # NOTE: is_part_of seems not to work in this context
+)
+
+# Check if there exists a model
+if solver.check() == sat:
+    model = solver.model()
+    print("Model found:")
+    for declaration in model.decls():
+        try: # do this to print bitvectors how we like to see them (as vectors)
+            print(f"{declaration.name()} = {model[declaration].sexpr()}")
+        except: # this is for the "else" case (ie, "map everything to x value")
+            print(f"{declaration.name()} = {model[declaration]}")
+        # NOTE: I don't think this prints all of what the model includes
+        # M: I think it works. rn it prints "possible = [else -> False]", which means map everything (ie, x and y) to false
+else:
+    print("No model found")
