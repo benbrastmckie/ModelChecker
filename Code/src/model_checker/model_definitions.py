4b24fb214453a0a49929e81accadcfff34462644
benbrastmckie <benbrastmckie@gmail.com>
2024-05-22 14:39:14 -0400

    seems to be working

--- Code/src/model_checker/model_definitions.py
+++ Code/src/model_checker/model_definitions.py
@@ -160,19 +160,17 @@ def atomic_propositions_dict_maker(state_space):
 def bit_fusion(bit_s, bit_t):
     """the result of taking the maximum for each index in _s and _t"""
     return simplify(bit_s | bit_t)
-    # NOTE: this does seem to make a difference, otherwise no comp_parts
+    # NOTE: 'simplify' does seem to make a difference, otherwise no comp_parts
 
 def bit_part(bit_s, bit_t):
     """the fusion of _s and _t is identical to bit_t"""
     return bool(simplify(bit_fusion(bit_s, bit_t) == bit_t))
-    # NOTE: this does seem to make a difference, otherwise no comp_parts
+    # NOTE: 'bool' does seem to make a difference, otherwise no comp_parts
 
 def bit_proper_part(bit_s, bit_t):
     """bit_s is a part of bit_t and bit_t is not a part of bit_s"""
     return bool(bit_part(bit_s, bit_t)) and not bit_s == bit_t
     # NOTE: this does not seem to make a difference and so has been turned off
-    # in the interest of discovering if it is required or not
-    # return bool(bit_part(bit_s, bit_t)) and not bit_s == bit_t
 
 def index_to_substate(index):
     '''
@@ -255,31 +253,6 @@ def bitvec_to_substates(bit_vec, N):
 #     disjoin_neg_conclusions = disjoin_prefix(neg_conclusions)
 #     return prefix_premises + disjoin_neg_conclusions
 
-def sentence_letters_in_compound(prefix_input_sentence):
-    """finds all the sentence letters in ONE input sentence. returns a list. WILL HAVE REPEATS
-    returns a list of AtomSorts. CRUCIAL: IN THAT SENSE DOES NOT FOLLOW SYNTAX OF PREFIX SENTS.
-    But that's ok, just relevant to know
-    used in all_sentence_letters
-    """
-    if len(prefix_input_sentence) == 1:  # base case: atomic sentence
-        return [prefix_input_sentence[0]] # redundant but conceptually clear
-    return_list = []
-    for part in prefix_input_sentence[1:]:
-        return_list.extend(sentence_letters_in_compound(part))
-    return return_list
-
-def all_sentence_letters(prefix_sentences):
-    """finds all the sentence letters in a list of input sentences.
-    returns as a list with no repeats (sorted for consistency)
-    used in find_all_constraints and StateSpace __init__"""
-    sentence_letters = set()
-    for prefix_input in prefix_sentences:
-        sentence_letters_in_input = sentence_letters_in_compound(prefix_input)
-        for sentence_letter in sentence_letters_in_input:
-            sentence_letters.add(sentence_letter)
-    return list(sentence_letters)
-    # sort just to make every output the same, given sets aren't hashable
-
 # def is_counterfactual(prefix_sentence):
 #     '''returns a boolean to say whether a given sentence is a counterfactual
 #     used in find_extensional_subsentences'''
@@ -345,6 +318,15 @@ def all_sentence_letters(prefix_sentences):
 #         return (extensional, modal, counterfactual, all_subsentences)
 #     return rr(return_list)
 
+def repeats_removed(sentences):
+    '''takes a list and removes the repeats in it.
+    used in find_all_constraints'''
+    seen = []
+    for obj in sentences:
+        if obj not in seen:
+            seen.append(obj)
+    return seen
+
 def subsentences_of(prefix_sentence):
     '''finds all the subsentence of a prefix sentence
     returns these as a set
@@ -369,21 +351,12 @@ def find_subsentences(prefix_sentences):
         all_subsentences.extend(all_prefix_subs)
     return repeats_removed(all_subsentences)
 
-def repeats_removed(sentences):
-    '''takes a list and removes the repeats in it.
-    used in find_all_constraints'''
-    seen = []
-    for obj in sentences:
-        if obj not in seen:
-            seen.append(obj)
-    return seen
-
-
 def evaluate_modal_expr(model_structure, prefix_modal, eval_world):
     '''evaluates whether a counterfatual in prefix form is true at a world (BitVecVal).
     used to initialize Counterfactuals
     returns a bool representing whether the counterfactual is true at the world or not'''
     op, argument = prefix_modal[0], prefix_modal[1]
+    # TODO: is this necessary?
     # if is_modal(argument):
     #     if model_structure.evaluate_modal_expr(prefix_modal) is True: # ie, verifiers is null state
     #         return True # both Box and Diamond will return true, since verifiers is not empty
