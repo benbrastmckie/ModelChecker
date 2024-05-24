f07b828036cf5d24e8bdcc081bc5844b3c486115
mfbuit46 <mfbuit46@mit.edu>
2024-05-24 18:24:52 -0500

    additions for use in python file

--- Code/src/model_checker/model_structure.py
+++ Code/src/model_checker/model_structure.py
@@ -149,6 +149,9 @@ class ModelSetup:
         model_runtime = round(model_end - model_start, 4)
         return (z3_model_status, z3_model, model_runtime)
 
+    def __str__(self):
+        return f'ModelSetup object with premises {self.infix_premises} and conclusions {self.infix_conclusions}'
+
 
 class ModelStructure:
     """self.premises is a list of prefix sentences
@@ -229,6 +232,19 @@ class ModelStructure:
         for index, con in enumerate(consts, start=1):
             print(f"{index}. {con}\n", file=output)
             # print(f"Constraints time: {time}\n")
+    
+    def __str__(self):
+        '''useful for people who want to use model_checker in a python file (like we use z3 or numpy, if that makes sense)
+        feel free to change/remove'''
+        return f'{"" if self.model_status else "un"}sat ModelStructure for premises {self.infix_premises} and conclusions {self.infix_conclusions} with status {self.model_status}'
+
+    def __bool__(self):
+        '''returns the value of self.model_status (ie, whether the z3 model was solved)
+        reasoning: say ms is a ModelStructure object. Now we can check its model_status by doing:
+        if ms: # as opposed to if ms.model_status
+            (do_something)
+        '''
+        return self.model_status
 
 
 class StateSpace:
@@ -381,6 +397,7 @@ class StateSpace:
 
     def print_states(self, print_impossible, output=sys.__stdout__):
         """print all fusions of atomic states in the model
+        print_impossible is a boolean for whether to print impossible states or not
         first print function in print.py"""
         N = self.model_setup.N
         print("\nState Space:", file=output)  # Print states
