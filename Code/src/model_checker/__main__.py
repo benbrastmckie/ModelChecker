9475cda23423b13084aa8bf813b32090e3b52120
benbrastmckie <benbrastmckie@gmail.com>
2024-07-15 15:29:13 -0400

    minor

--- Code/src/model_checker/__main__.py
+++ Code/src/model_checker/__main__.py
@@ -16,17 +16,17 @@ import importlib.util
 # didn't work
 # sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# Get the directory path of the current file
-current_dir = os.path.dirname(__file__)
-# Construct the full path to your project root
-project_root = os.path.abspath(os.path.join(current_dir, ".."))
-# project_root = project_root[:-4] # bandaid fix to remove "/src" from the root
-# Add the project root to the Python path
-sys.path.append(project_root)
+# # Get the directory path of the current file
+# current_dir = os.path.dirname(__file__)
+# # Construct the full path to your project root
+# project_root = os.path.abspath(os.path.join(current_dir, ".."))
+# # project_root = project_root[:-4] # bandaid fix to remove "/src" from the root
+# # Add the project root to the Python path
+# sys.path.append(project_root)
 
 ### FOR TESTING ###
 from __init__ import __version__
-from model_structure import ( # for packaging
+from model_structure import (
     StateSpace,
     make_model_for,
     )
