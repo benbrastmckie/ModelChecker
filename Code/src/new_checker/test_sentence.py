c6265218f6ab8bed62e614e17300a8788482396e
benbrastmckie <benbrastmckie@gmail.com>
2024-10-12 11:53:50 -0400

    fixed Sentence methods

--- Code/src/new_checker/test_sentence.py
+++ Code/src/new_checker/test_sentence.py
@@ -5,12 +5,12 @@ from syntactic import(
 
 
 # COMPLEXITY
-example = "((A \\rightarrow (B \\wedge C)) \\wedge (A \\rightarrow (B \\wedge C)))"
+example = "((A \\rightarrow (D \\wedge C)) \\wedge (A \\rightarrow (B \\wedge C)))"
 sent = Sentence(example)
 prefix = sent.prefix(example)
 print(prefix)
 letters, subs, comp = sent.constituents_of(prefix)
-print(f"{example} has sentence letters {letters}")
-print(f"{example} has subsentences {subs}")
+print(f"{example} has {len(letters)} sentence letters {letters}")
+print(f"{example} has {len(subs)} subsentences {subs}")
 print(f"{example} has complexity {comp}")
 
