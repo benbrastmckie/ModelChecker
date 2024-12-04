7d9ab9a07f6e7094e9cf668d2025862ed8d0e321
benbrastmckie <benbrastmckie@gmail.com>
2024-12-04 11:26:20 -0500

    added imposition semantics; preliminary tests show that it is much slower

--- Code/src/new_checker/primitive_operators.py
+++ Code/src/new_checker/primitive_operators.py
@@ -625,6 +625,70 @@ class CounterfactualOperator(syntactic.Operator):
         self.print_over_worlds(sentence_obj, eval_world, alt_worlds, indent_num)
 
 
+class ImpositionOperator(syntactic.Operator):
+    name = "\\imposition"
+    arity = 2
+
+    def true_at(self, leftarg, rightarg, eval_world):
+        sem = self.semantics
+        N = sem.N
+        x = z3.BitVec("t_imp_x", N)
+        u = z3.BitVec("t_imp_u", N)
+        return ForAll(
+            [x, u],
+            z3.Implies(
+                z3.And(
+                    sem.extended_verify(x, leftarg, eval_world),
+                    sem.imposition(x, eval_world, u)
+                ),
+                sem.true_at(rightarg, u),
+            ),
+        )
+    
+    def false_at(self, leftarg, rightarg, eval_world):
+        sem = self.semantics
+        N = sem.N
+        x = z3.BitVec("f_imp_x", N)
+        u = z3.BitVec("f_imp_u", N)
+        return Exists(
+            [x, u],
+            z3.And(
+                sem.extended_verify(x, leftarg, eval_world),
+                sem.imposition(x, eval_world, u),
+                sem.false_at(rightarg, u)),
+            )
+
+    def extended_verify(self, state, leftarg, rightarg, eval_world):
+        # TODO: add constraint which requires state to be the null_bit
+        return self.true_at(leftarg, rightarg, eval_world)
+    
+    def extended_falsify(self, state, leftarg, rightarg, eval_world):
+        # TODO: add constraint which requires state to be the null_bit
+        return self.false_at(leftarg, rightarg, eval_world)
+
+    def find_verifiers_and_falsifiers(self, leftarg, rightarg, eval_world):
+        evaluate = leftarg.proposition.model_structure.z3_model.evaluate
+        if bool(evaluate(self.true_at(leftarg, rightarg, eval_world))):
+            return {self.semantics.null_bit}, set()
+        if bool(evaluate(self.false_at(leftarg, rightarg, eval_world))):
+            return set(), {self.semantics.null_bit}
+        raise ValueError(
+            f"{leftarg.name} {self.name} {rightarg.name} "
+            f"is neither true nor false in the world {eval_world}."
+        )
+    
+    def print_method(self, sentence_obj, eval_world, indent_num):
+        """Print counterfactual and the antecedent in the eval_world. Then
+        print the consequent in each alternative to the evaluation world.
+        """
+        is_outcome = self.semantics.calculate_outcome_worlds
+        model_structure = sentence_obj.proposition.model_structure
+        left_argument_obj = sentence_obj.original_arguments[0]
+        left_argument_verifiers = left_argument_obj.proposition.verifiers
+        alt_worlds = is_outcome(left_argument_verifiers, eval_world, model_structure)
+        self.print_over_worlds(sentence_obj, eval_world, alt_worlds, indent_num)
+
+
 
 ##############################################################################
 ########################### INTENSIONAL OPERATORS ############################
