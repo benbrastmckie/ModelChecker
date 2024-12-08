4e19507b3b9088613232167ef51ee40edf050047
benbrastmckie <benbrastmckie@gmail.com>
2024-12-07 21:36:25 -0500

    got comparison running

--- Code/src/new_checker/example.py
+++ Code/src/new_checker/example.py
@@ -2,6 +2,7 @@ from example_builder import (
     compare_semantics,
     make_model_for,
     find_max_N,
+    translate,
 )
  
 from semantic import (
@@ -75,8 +76,8 @@ settings = {
 # premises = ["A", "((\\neg \\top \\rightarrow (B \\wedge C)) \\wedge D)"]
 # premises = ["(A \\rightarrow B)", "A"]
 # premises = ["(A \\rightarrow (B \\rightarrow C))", "D"]
-# premises = ["(A \\leftrightarrow B)", "\\possible A"]
-# premises = ["\\possible B"]
+# premises = ["(A \\leftrightarrow B)", "\\Diamond A"]
+# premises = ["\\Diamond B"]
 # premises = ["\\Box A", "(A \\leftrightarrow B)"]
 # premises = ["\\Box \\Box A"]
 # premises = ["\\necessary \\neg \\neg A", "\\neg \\neg \\neg \\necessary B", "B"]
@@ -125,145 +126,223 @@ settings = {
 
 # # CF_CM1: COUNTERFACTUAL ANTECEDENT STRENGTHENING
 # N = 3
-# premises = ['(A \\boxright C)']
-# conclusions = ['((A \\wedge B) \\boxright C)']
+CF_CM1_premises = ['(A \\boxright C)']
+CF_CM1_conclusions = ['((A \\wedge B) \\boxright C)']
+CF_CM1_example = [
+    CF_CM1_premises,
+    CF_CM1_conclusions,
+]
 # settings['continget'] = True
 
 # # CF_CM2: MIGHT COUNTERFACTUAL ANTECEDENT STRENGTHENING
 # N = 3
-# premises = ['(A \\circleright C)']
-# conclusions = ['((A \\wedge B) \\circleright C)']
+CF_CM2_premises = ['(A \\circleright C)']
+CF_CM2_conclusions = ['((A \\wedge B) \\circleright C)']
+CF_CM2_example = [
+    CF_CM2_premises,
+    CF_CM2_conclusions,
+]
 # settings['continget'] = True
 
 # # CF_CM3: COUNTERFACTUAL ANTECEDENT STRENGTHENING WITH POSSIBILITY
 # N = 3
-# premises = ['(A \\boxright C)', '\\possible (A \\wedge B)']
-# conclusions = ['((A \\wedge B) \\boxright C)']
+CF_CM3_premises = ['(A \\boxright C)', '\\Diamond (A \\wedge B)']
+CF_CM3_conclusions = ['((A \\wedge B) \\boxright C)']
+CF_CM3_example = [
+    CF_CM3_premises,
+    CF_CM3_conclusions,
+]
 
 # # CF_CM4: COUNTERFACTUAL ANTECEDENT STRENGTHENING WITH NEGATION
 # N = 4
-# premises = ['\\neg A','(A \\boxright C)']
-# conclusions = ['((A \\wedge B) \\boxright C)']
+CF_CM4_premises = ['\\neg A','(A \\boxright C)']
+CF_CM4_conclusions = ['((A \\wedge B) \\boxright C)']
+CF_CM4_example = [
+    CF_CM4_premises,
+    CF_CM4_conclusions,
+]
 
 # # CF_CM5: COUNTERFACTUAL DOUBLE ANTECEDENT STRENGTHENING
 # N = 4
-# premises = ['(A \\boxright C)','(B \\boxright C)']
-# conclusions = ['((A \\wedge B) \\boxright C)']
+CF_CM5_premises = ['(A \\boxright C)','(B \\boxright C)']
+CF_CM5_conclusions = ['((A \\wedge B) \\boxright C)']
+CF_CM5_example = [
+    CF_CM5_premises,
+    CF_CM5_conclusions,
+]
 
 # # CF_CM6: WEAKENED MONOTONICITY
 # N = 3
-# premises = ['(A \\boxright B)','(A \\boxright C)']
-# conclusions = ['((A \\wedge B) \\boxright C)']
-# settings['contingent'] = False
-
-# # IMP_CM6: WEAKENED MONOTONIC IMPOSITION
-# N = 3
-# premises = ['(A \\imposition B)','(A \\imposition C)']
-# conclusions = ['((A \\wedge B) \\imposition C)']
+CF_CM6_premises = ['(A \\boxright B)','(A \\boxright C)']
+CF_CM6_conclusions = ['((A \\wedge B) \\boxright C)']
+CF_CM6_example = [
+    CF_CM6_premises,
+    CF_CM6_conclusions,
+]
 # settings['contingent'] = False
 
 # # CF_CM7: COUNTERFACTUAL CONTRAPOSITION
 # N = 3
-# premises = ['(A \\boxright B)']
-# conclusions = ['(\\neg B \\boxright \\neg A)']
+CF_CM7_premises = ['(A \\boxright B)']
+CF_CM7_conclusions = ['(\\neg B \\boxright \\neg A)']
+CF_CM7_example = [
+    CF_CM7_premises,
+    CF_CM7_conclusions,
+]
 
 # # CF_CM8: COUNTERFACTUAL CONTRAPOSITION WITH NEGATION
 # # NOTE: with Z3 quantifiers ran for 125 seconds on the MIT server; now .181 seconds locally
 # N = 4
-# premises = ['\\neg B','(A \\boxright B)']
-# conclusions = ['(\\neg B \\boxright \\neg A)']
+CF_CM8_premises = ['\\neg B','(A \\boxright B)']
+CF_CM8_conclusions = ['(\\neg B \\boxright \\neg A)']
+CF_CM8_example = [
+    CF_CM8_premises,
+    CF_CM8_conclusions,
+]
 
 # # CF_CM9: COUNTERFACTUAL CONTRAPOSITION WITH TWO NEGATIONS
 # N = 4
-# premises = ['\\neg A','\\neg B','(A \\boxright B)']
-# conclusions = ['(\\neg B \\boxright \\neg A)']
+CF_CM9_premises = ['\\neg A','\\neg B','(A \\boxright B)']
+CF_CM9_conclusions = ['(\\neg B \\boxright \\neg A)']
+CF_CM9_example = [
+    CF_CM9_premises,
+    CF_CM9_conclusions,
+]
 
 # # CF_CM10: TRANSITIVITY
 # N = 3
-# premises = ['(A \\boxright B)','(B \\boxright C)']
-# conclusions = ['(A \\boxright C)']
+CF_CM10_premises = ['(A \\boxright B)','(B \\boxright C)']
+CF_CM10_conclusions = ['(A \\boxright C)']
+CF_CM10_example = [
+    CF_CM10_premises,
+    CF_CM10_conclusions,
+]
 
 # # CF_CM11: COUNTERFACTUAL TRANSITIVITY WITH NEGATION
 # N = 3
-# premises = ['\\neg A','(A \\boxright B)','(B \\boxright C)']
-# conclusions = ['(A \\boxright C)']
+CF_CM11_premises = ['\\neg A','(A \\boxright B)','(B \\boxright C)']
+CF_CM11_conclusions = ['(A \\boxright C)']
+CF_CM11_example = [
+    CF_CM11_premises,
+    CF_CM11_conclusions,
+]
 
 # # CF_CM12: COUNTERFACTUAL TRANSITIVITY WITH TWO NEGATIONS
 # N = 4
-# premises = ['\\neg A','\\neg B','(A \\boxright B)','(B \\boxright C)']
-# conclusions = ['(A \\boxright C)']
+CF_CM12_premises = ['\\neg A','\\neg B','(A \\boxright B)','(B \\boxright C)']
+CF_CM12_conclusions = ['(A \\boxright C)']
+CF_CM12_example = [
+    CF_CM12_premises,
+    CF_CM12_conclusions,
+]
 
 # # CF_CM13: SOBEL SEQUENCE
 # N = 3
-# premises = [
-#     '(A \\boxright X)',
-#     '\\neg ((A \\wedge B) \\boxright X)',
-#     '(((A \\wedge B) \\wedge C) \\boxright X)',
-#     '\\neg ((((A \\wedge B) \\wedge C) \\wedge D) \\boxright X)',
-#     '(((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\boxright X)',
-#     '\\neg ((((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\wedge F) \\boxright X)',
-#     '(((((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\wedge F) \\wedge G) \\boxright X)', # 327.2 seconds on the MIT servers; now .01244 seconds
-# ]
-# conclusions = []
+CF_CM13_premises = [
+    '(A \\boxright X)',
+    '\\neg ((A \\wedge B) \\boxright X)',
+    '(((A \\wedge B) \\wedge C) \\boxright X)',
+    '\\neg ((((A \\wedge B) \\wedge C) \\wedge D) \\boxright X)',
+    '(((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\boxright X)',
+    '\\neg ((((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\wedge F) \\boxright X)',
+    '(((((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\wedge F) \\wedge G) \\boxright X)', # 327.2 seconds on the MIT servers; now .01244 seconds
+]
+CF_CM13_conclusions = []
+CF_CM13_example = [
+    CF_CM13_premises,
+    CF_CM13_conclusions,
+]
 
 # # CF_CM14: SOBEL SEQUENCE WITH POSSIBILITY (N = 3)
 # N = 3
-# premises = [
-#     '\\possible A',
-#     '(A \\boxright X)',
-#     '\\possible (A \\wedge B)',
-#     '\\neg ((A \\wedge B) \\boxright X)', # N = 4: 155.4 seconds on the MIT servers; .1587 seconds in old version; and now .0122 seconds
-#     '\\possible ((A \\wedge B) \\wedge C)',
-#     '(((A \\wedge B) \\wedge C) \\boxright X)',
-#     '\\possible (((A \\wedge B) \\wedge C) \\wedge D)',
-#     '\\neg ((((A \\wedge B) \\wedge C) \\wedge D) \\boxright X)',
-#     '\\possible ((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E)',
-#     '(((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\boxright X)', # ? seconds
-#     '\\possible (((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\wedge F)',
-#     '\\neg ((((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\wedge F) \\boxright X)', # ? seconds
-#     '\\possible ((((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\wedge F) \\wedge G)',
-#     '(((((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\wedge F) \\wedge G) \\boxright X)', # ? seconds
-# ]
-# conclusions = []
+CF_CM14_premises = [
+    '\\Diamond A',
+    '(A \\boxright X)',
+    '\\Diamond (A \\wedge B)',
+    '\\neg ((A \\wedge B) \\boxright X)', # N = 4: 155.4 seconds on the MIT servers; .1587 seconds in old version; and now .0122 seconds
+    '\\Diamond ((A \\wedge B) \\wedge C)',
+    '(((A \\wedge B) \\wedge C) \\boxright X)',
+    '\\Diamond (((A \\wedge B) \\wedge C) \\wedge D)',
+    '\\neg ((((A \\wedge B) \\wedge C) \\wedge D) \\boxright X)',
+    '\\Diamond ((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E)',
+    '(((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\boxright X)', # ? seconds
+    '\\Diamond (((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\wedge F)',
+    '\\neg ((((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\wedge F) \\boxright X)', # ? seconds
+    '\\Diamond ((((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\wedge F) \\wedge G)',
+    '(((((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\wedge F) \\wedge G) \\boxright X)', # ? seconds
+]
+CF_CM14_conclusions = []
+CF_CM14_example = [
+    CF_CM14_premises,
+    CF_CM14_conclusions,
+]
 
 # # CF_CM15: COUNTERFACTUAL EXCLUDED MIDDLE
 # N = 3
-# premises = ['\\neg A']
-# conclusions = ['(A \\boxright B)','(A \\boxright \\neg B)']
+CF_CM15_premises = ['\\neg A']
+CF_CM15_conclusions = ['(A \\boxright B)','(A \\boxright \\neg B)']
+CF_CM15_example = [
+    CF_CM15_premises,
+    CF_CM15_conclusions,
+]
 
 # # CF_CM16: SIMPLIFICATION OF DISJUNCTIVE CONSEQUENT
 # N = 3
-# premises = ['\\neg A','(A \\boxright (B \\vee C))']
-# conclusions = ['(A \\boxright B)','(A \\boxright C)']
+CF_CM16_premises = ['\\neg A','(A \\boxright (B \\vee C))']
+CF_CM16_conclusions = ['(A \\boxright B)','(A \\boxright C)']
+CF_CM16_example = [
+    CF_CM16_premises,
+    CF_CM16_conclusions,
+]
 
 # # CF_CM17: INTRODUCTION OF DISJUNCTIVE ANTECEDENT
 # N = 4
-# premises = ['(A \\boxright C)','(B \\boxright C)']
-# conclusions = ['((A \\vee B) \\boxright C)']
+CF_CM17_premises = ['(A \\boxright C)','(B \\boxright C)']
+CF_CM17_conclusions = ['((A \\vee B) \\boxright C)']
+CF_CM17_example = [
+    CF_CM17_premises,
+    CF_CM17_conclusions,
+]
 # settings['continget'] = True
 
 # # CF_CM18: MUST FACTIVITY
 # N = 3
-# premises = ['A','B']
-# conclusions = ['(A \\boxright B)']
+CF_CM18_premises = ['A','B']
+CF_CM18_conclusions = ['(A \\boxright B)']
+CF_CM18_example = [
+    CF_CM18_premises,
+    CF_CM18_conclusions,
+]
 # settings['continget'] = True
 
 # # CF_CM19: COUNTERFACTUAL EXPORTATION
 # N = 3
-# premises = ['((A \\wedge B) \\boxright C)']
-# conclusions = ['(A \\boxright (B \\boxright C))']
+CF_CM19_premises = ['((A \\wedge B) \\boxright C)']
+CF_CM19_conclusions = ['(A \\boxright (B \\boxright C))']
+CF_CM19_example = [
+    CF_CM19_premises,
+    CF_CM19_conclusions,
+]
 # settings['continget'] = True
 
 # # CF_CM20: COUNTERFACTUAL EXPORTATION WITH POSSIBILITY
 # N = 3
-# premises = ['((A \\wedge B) \\boxright C)','\\possible (A \\wedge B)']
-# conclusions = ['(A \\boxright (B \\boxright C))']
+CF_CM20_premises = ['((A \\wedge B) \\boxright C)','\\Diamond (A \\wedge B)']
+CF_CM20_conclusions = ['(A \\boxright (B \\boxright C))']
+CF_CM20_example = [
+    CF_CM20_premises,
+    CF_CM20_conclusions,
+]
 # settings['continget'] = True
 
-# # CF_CM21
+# # CF_CM21:
 # N = 3
-# premises = ['\\neg A','\\neg (A \\boxright B)']
-# conclusions = ['(A \\boxright \\neg B)']
+CF_CM21_premises = ['\\neg A','\\neg (A \\boxright B)']
+CF_CM21_conclusions = ['(A \\boxright \\neg B)']
+CF_CM21_example = [
+    CF_CM21_premises,
+    CF_CM21_conclusions
+]
 # settings['continget'] = True
 
 
@@ -370,32 +449,67 @@ settings = {
 #     settings,
 # )
 
-premises = ['(A \\boxright C)']
-conclusions = ['((A \\wedge B) \\boxright C)']
+CM_examples = [
+    CF_CM1_example,
+    CF_CM2_example,
+    CF_CM3_example,
+    CF_CM4_example,
+    CF_CM5_example,
+    CF_CM6_example,
+    CF_CM7_example,
+    CF_CM8_example,
+    CF_CM9_example,
+    CF_CM10_example,
+    CF_CM11_example,
+    CF_CM12_example,
+    CF_CM13_example,
+    CF_CM14_example,
+    CF_CM15_example,
+    CF_CM16_example,
+    CF_CM17_example,
+    CF_CM18_example,
+    CF_CM19_example,
+    CF_CM20_example,
+    CF_CM21_example,
+]
 
-imp_prems = ['(A \\imposition C)']
-imp_cons = ['((A \\wedge B) \\imposition C)']
+for example in CM_examples:
+    print(f"Example: {str(example)}")
+    premises, conclusions = example
+    print(f"  Premises:")
+    for prem in premises:
+        print(f"    {prem}")
+    print(f"  Conclusions:")
+    for con in conclusions:
+        print(f"    {con}")
 
-default_theory = [
-    premises,
-    conclusions,
-    Semantics,
-    Proposition,
-    operators,
-    settings,
-]
-imposition_theory = [
-    imp_prems,
-    imp_cons,
-    ImpositionSemantics,
-    Proposition,
-    operators,
-    settings,
-]
-theory_list = [
-    default_theory,
-    imposition_theory,
-]
-compare_semantics(theory_list)
+    dictionary = {
+        '\\boxright' : '\\imposition',
+        '\\circleright' : '\\could',
+    }
+
+    alt_premises, alt_conclusions = translate(premises, conclusions, dictionary)
+
+    default_theory = [
+        premises,
+        conclusions,
+        Semantics,
+        Proposition,
+        operators,
+        settings,
+    ]
+    imposition_theory = [
+        alt_premises,
+        alt_conclusions,
+        ImpositionSemantics,
+        Proposition,
+        operators,
+        settings,
+    ]
+    theory_list = [
+        default_theory,
+        imposition_theory,
+    ]
+    compare_semantics(theory_list)
 
 
