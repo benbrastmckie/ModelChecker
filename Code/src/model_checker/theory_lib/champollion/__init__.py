4fa7d40c98f0b8382b1bbb7b45b504176707e7e0
benbrastmckie <benbrastmckie@gmail.com>
2025-01-20 20:41:25 -0800

    fixed init

---
+++ Code/src/model_checker/theory_lib/champollion/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "0.0.1"
+
+# Import specific items from semantic
+from .semantic import (
+    ChampollionSemantics,
+    ChampollionProposition,
+)
+
+# Import specific items from primitive
+from .operators import (
+    UniAndOperator, UniOrOperator, ExclusionOperator, # extensional
+    UniIdentityOperator, # constitutive
+)
+
+from .examples import (
+    example_range,
+    semantic_theories,
+    ) 
