baeeedfa54ffc2290865bd356474ab0defc2d246
benbrastmckie <benbrastmckie@gmail.com>
2025-02-15 21:50:51 -0500

    started bimodal semantics

---
+++ Code/src/model_checker/theory_lib/intensional/examples.py
@@ -0,0 +1,397 @@
+"""
+INSTRUCTIONS: this module defines the semantic_theories and example_range.
+From the project directory, run: model_checker examples.py
+"""
+
+##########################
+### DEFINE THE IMPORTS ###
+##########################
+
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))  # Add the current directory to sys.path
+
+from semantic import (
+    Semantics,
+    Proposition,
+)
+
+from operators import (
+    default_operators,
+)
+
+
+########################
+### DEFAULT SETTINGS ###
+########################
+
+general_settings = {
+    "print_constraints": False,
+    "print_impossible": False,
+    "print_z3": False,
+    "save_output": False,
+    "maximize": False,
+}
+
+example_settings = {  # defaults can be tailored to each example
+    'N' : 3,
+    'contingent' : False,
+    'disjoint' : False,
+    'non_empty' : False,
+    'non_null' : False,
+    'max_time' : 1,
+}
+
+
+####################################
+### DEFINE THE SEMANTIC THEORIES ###
+####################################
+
+default_theory = {
+    "semantics": Semantics,
+    "proposition": Proposition,
+    "operators": default_operators,
+    # default theory does not require a translation dictionary for comparison
+    # since the examples are stated in the language of the default theory
+}
+
+
+#####################
+### COUNTERMODELS ###
+#####################
+
+# CF_CM_1: COUNTERFACTUAL ANTECEDENT STRENGTHENING
+CF_CM_1_premises = ['(A \\boxright C)']
+CF_CM_1_conclusions = ['((A \\wedge B) \\boxright C)']
+CF_CM_1_settings = {
+    'N' : 3,
+    'contingent' : True,
+    'non_null' : True,
+    'non_empty' : True,
+    'disjoint' : False,
+    'max_time' : 1,
+}
+CF_CM_1_example = [
+    CF_CM_1_premises,
+    CF_CM_1_conclusions,
+    CF_CM_1_settings,
+]
+
+# CF_CM_2: MIGHT COUNTERFACTUAL ANTECEDENT STRENGTHENING
+CF_CM_2_premises = ['(A \\circleright C)']
+CF_CM_2_conclusions = ['((A \\wedge B) \\circleright C)']
+CF_CM_2_settings = {
+    'N' : 4,
+    'contingent' : True,
+    'non_null' : True,
+    'non_empty' : True,
+    'disjoint' : False,
+    'max_time' : 1,
+}
+CF_CM_2_example = [
+    CF_CM_2_premises,
+    CF_CM_2_conclusions,
+    CF_CM_2_settings,
+]
+
+# CF_CM_7: COUNTERFACTUAL CONTRAPOSITION
+CF_CM_7_premises = ['(A \\boxright B)']
+CF_CM_7_conclusions = ['(\\neg B \\boxright \\neg A)']
+CF_CM_7_settings = {
+    'N' : 4,
+    'contingent' : True,
+    'non_null' : True,
+    'non_empty' : True,
+    'disjoint' : False,
+    'max_time' : 1,
+}
+CF_CM_7_example = [
+    CF_CM_7_premises,
+    CF_CM_7_conclusions,
+    CF_CM_7_settings,
+]
+
+# CF_CM_10: TRANSITIVITY
+CF_CM_10_premises = ['(A \\boxright B)','(B \\boxright C)']
+CF_CM_10_conclusions = ['(A \\boxright C)']
+CF_CM_10_settings = {
+    'N' : 4,
+    'contingent' : True,
+    'non_null' : True,
+    'non_empty' : True,
+    'disjoint' : False,
+    'max_time' : 1,
+}
+CF_CM_10_example = [
+    CF_CM_10_premises,
+    CF_CM_10_conclusions,
+    CF_CM_10_settings,
+]
+
+# CF_CM_13: SOBEL SEQUENCE
+CF_CM_13_premises = [
+    '(A \\boxright X)',
+    '\\neg ((A \\wedge B) \\boxright X)',
+    '(((A \\wedge B) \\wedge C) \\boxright X)',
+    '\\neg ((((A \\wedge B) \\wedge C) \\wedge D) \\boxright X)',
+    '(((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\boxright X)',
+    '\\neg ((((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\wedge F) \\boxright X)',
+    '(((((((A \\wedge B) \\wedge C) \\wedge D) \\wedge E) \\wedge F) \\wedge G) \\boxright X)', # 327.2 seconds on the MIT servers; now .01244 seconds
+]
+CF_CM_13_conclusions = []
+CF_CM_13_settings = {
+    'N' : 4,
+    'contingent' : True,
+    'non_null' : True,
+    'non_empty' : True,
+    'disjoint' : False,
+    'max_time' : 1,
+}
+CF_CM_13_example = [
+    CF_CM_13_premises,
+    CF_CM_13_conclusions,
+    CF_CM_13_settings,
+]
+
+### CONSTITUTIVE OPERATORS ###
+
+# CL_CM_3: GROUND CONJUNCTION SUPPLEMENTATION
+CL_CM_3_premises = ['(A \\leq B)','(C \\leq D)']
+CL_CM_3_conclusions = ['((A \\wedge C) \\leq (B \\wedge D))']
+CL_CM_3_settings = {
+    'N' : 3,
+    'contingent' : True,
+    'non_null' : True,
+    'non_empty' : True,
+    'disjoint' : False,
+    'max_time' : 1,
+}
+CL_CM_3_example = [
+    CL_CM_3_premises,
+    CL_CM_3_conclusions,
+    CL_CM_3_settings,
+]
+
+# CL_CM_4: ESSENCE DISJUNCTION SUPPLEMENTATION
+CL_CM_4_premises = ['(A \\sqsubseteq B)','(C \\sqsubseteq D)']
+CL_CM_4_conclusions = ['((A \\vee C) \\sqsubseteq (B \\vee D))']
+CL_CM_4_settings = {
+    'N' : 3,
+    'contingent' : True,
+    'non_null' : True,
+    'non_empty' : True,
+    'disjoint' : False,
+    'max_time' : 1,
+}
+CL_CM_4_example = [
+    CL_CM_4_premises,
+    CL_CM_4_conclusions,
+    CL_CM_4_settings,
+]
+
+# CL_CM_5: IDENTITY ABSORPTION: DISJUNCTION OVER CONJUNCTION
+CL_CM_5_premises = []
+CL_CM_5_conclusions = ['(A \\equiv (A \\vee (A \\wedge B)))']
+CL_CM_5_settings = {
+    'N' : 3,
+    'contingent' : True,
+    'non_null' : True,
+    'non_empty' : True,
+    'disjoint' : False,
+    'max_time' : 1,
+}
+CL_CM_5_example = [
+    CL_CM_5_premises,
+    CL_CM_5_conclusions,
+    CL_CM_5_settings,
+]
+
+# CL_CM_6: IDENTITY ABSORPTION: DISJUNCTION OVER CONJUNCTION
+CL_CM_6_premises = []
+CL_CM_6_conclusions = ['(A \\equiv (A \\wedge (A \\vee B)))']
+CL_CM_6_settings = {
+    'N' : 3,
+    'contingent' : True,
+    'non_null' : True,
+    'non_empty' : True,
+    'disjoint' : False,
+    'max_time' : 1,
+}
+CL_CM_6_example = [
+    CL_CM_6_premises,
+    CL_CM_6_conclusions,
+    CL_CM_6_settings,
+]
+
+# CL_CM_10: IDENTITY DISTRIBUTION
+CL_CM_10_premises = []
+CL_CM_10_conclusions = ['((A \\vee (B \\wedge C)) \\equiv ((A \\vee B) \\wedge (A \\vee C)))']
+CL_CM_10_settings = {
+    'N' : 3,
+    'contingent' : True,
+    'non_null' : True,
+    'non_empty' : True,
+    'disjoint' : False,
+    'max_time' : 1,
+}
+CL_CM_10_example = [
+    CL_CM_10_premises,
+    CL_CM_10_conclusions,
+    CL_CM_10_settings,
+]
+
+
+
+############################
+### LOGICAL CONSEQUENCES ###
+############################
+
+# CF_TH_2: COUNTERFACTUAL MODUS PONENS
+CF_TH_2_premises = ['A','(A \\boxright B)']
+CF_TH_2_conclusions = ['B']
+CF_TH_2_example = [
+    CF_TH_2_premises,
+    CF_TH_2_conclusions,
+    example_settings,  # can use example_settings from above
+]
+
+# CF_TH_3: WEAKENED TRANSITIVITY
+CF_TH_3_premises = ['(A \\boxright B)','((A \\wedge B) \\boxright C)']
+CF_TH_3_conclusions = ['(A \\boxright C)']
+CF_TH_3_example = [
+    CF_TH_3_premises,
+    CF_TH_3_conclusions,
+    example_settings,
+]
+
+# CF_TH_5: SIMPLIFICATION OF DISJUNCTIVE ANTECEDENT
+CF_TH_5_premises = ['((A \\vee B) \\boxright C)']
+CF_TH_5_conclusions = ['(A \\boxright C)']
+CF_TH_5_example = [
+    CF_TH_5_premises,
+    CF_TH_5_conclusions,
+    example_settings,
+]
+
+# CF_TH_10: MIGHT FACTIVITY
+CF_TH_10_settings = example_settings  # copy defaults
+CF_TH_10_settings['N'] = 4  # change atomic number
+CF_TH_10_premises = ['A','B']
+CF_TH_10_conclusions = ['(A \\circleright B)']
+CF_TH_10_example = [
+    CF_TH_10_premises,
+    CF_TH_10_conclusions,
+    CF_TH_10_settings,
+]
+
+# CF_TH_11: DEFINITION OF NEC
+CF_TH_11_premises = ['(\\neg A \\boxright \\bot)']
+CF_TH_11_conclusions = ['(\\top \\boxright A)']
+CF_TH_11_example = [
+    CF_TH_11_premises,
+    CF_TH_11_conclusions,
+    example_settings,
+]
+
+### CONSTITUTIVE OPERATORS ###
+
+# CL_TH_1: GROUND TO ESSENCE
+CL_TH_1_premises = ['(A \\leq B)']
+CL_TH_1_conclusions = ['(\\neg A \\sqsubseteq \\neg B)']
+CL_TH_1_example = [
+    CL_TH_1_premises,
+    CL_TH_1_conclusions,
+    example_settings,
+]
+
+# CL_TH_2: ESSENCE TO GROUND 
+CL_TH_2_premises = ['(A \\sqsubseteq B)']
+CL_TH_2_conclusions = ['(\\neg A \\leq \\neg B)']
+CL_TH_2_example = [
+    CL_TH_2_premises,
+    CL_TH_2_conclusions,
+    example_settings,
+]
+
+# CL_TH_3: ESSENCE TO IDENTITY
+CL_TH_3_premises = ['(A \\sqsubseteq B)']
+CL_TH_3_conclusions = ['((A \\wedge B) \\equiv B)']
+CL_TH_3_example = [
+    CL_TH_3_premises,
+    CL_TH_3_conclusions,
+    example_settings,
+]
+
+# CL_TH_4: IDENTITY TO ESSENCE 
+CL_TH_4_premises = ['((A \\wedge B) \\equiv B)']
+CL_TH_4_conclusions = ['(A \\sqsubseteq B)']
+CL_TH_4_example = [
+    CL_TH_4_premises,
+    CL_TH_4_conclusions,
+    example_settings,
+]
+
+# CL_TH_5: GROUND TO IDENTITY
+CL_TH_5_premises = ['(A \\leq B)']
+CL_TH_5_conclusions = ['((A \\vee B) \\equiv B)']
+CL_TH_5_example = [
+    CL_TH_5_premises,
+    CL_TH_5_conclusions,
+    example_settings,
+]
+
+# CL_TH_6: IDENTITY TO GROUND 
+CL_TH_6_premises = ['((A \\vee B) \\equiv B)']
+CL_TH_6_conclusions = ['(A \\leq B)']
+CL_TH_6_example = [
+    CL_TH_6_premises,
+    CL_TH_6_conclusions,
+    example_settings,
+]
+
+# CL_TH_7: NEGATION TRANSPARENCY
+CL_TH_7_premises = ['(A \\equiv B)']
+CL_TH_7_conclusions = ['(\\neg A \\equiv \\neg B)']
+CL_TH_7_example = [
+    CL_TH_7_premises,
+    CL_TH_7_conclusions,
+    example_settings,
+]
+
+# CL_TH_8: REVERSE NEGATION TRANSPARENCY
+CL_TH_8_premises = ['(\\neg A \\equiv \\neg B)']
+CL_TH_8_conclusions = ['(A \\equiv B)']
+CL_TH_8_example = [
+    CL_TH_8_premises,
+    CL_TH_8_conclusions,
+    example_settings,
+]
+
+
+
+###############################################
+### DEFINE EXAMPLES AND THEORIES TO COMPUTE ###
+###############################################
+
+# NOTE: at least one theory is required, multiple are permitted for comparison
+semantic_theories = {
+    "Brast-McKie" : default_theory,
+    # additional theories will require their own translation dictionaries
+}
+
+# NOTE: at least one example is required, multiple are permitted for comparison
+example_range = {
+    # Counterfactual Countermodels
+    "CF_CM_1" : CF_CM_1_example,
+    # "CF_CM_2" : CF_CM_2_example,
+    # "CF_CM_7" : CF_CM_7_example,
+    # "CF_CM_10" : CF_CM_10_example,
+    # "CF_CM_13" : CF_CM_13_example,
+
+    # Counterfactual Theorems
+    # "CF_TH_2" : CF_TH_2_example,
+    # "CF_TH_3" : CF_TH_3_example,
+    # "CF_TH_5" : CF_TH_5_example,
+    # "CF_TH_10" : CF_TH_10_example,
+    # "CF_TH_11" : CF_TH_11_example,
+}
---
+++ Code/src/model_checker/theory_lib/intensional/semantic.py
@@ -0,0 +1,264 @@
+import z3
+
+from model_checker.model import (
+    IntensionalSemanticDefaults,
+    IntensionalPropositionDefaults,
+)
+
+from model_checker.utils import (
+    ForAll,
+    Exists,
+    bitvec_to_substates,
+)
+
+from model_checker import syntactic
+
+
+##############################################################################
+######################### SEMANTICS AND PROPOSITIONS #########################
+##############################################################################
+
+
+class Semantics(IntensionalSemanticDefaults):
+    """Defines the semantic model for bimodal logic, including primitive relations,
+    frame constraints for task transitions between world states, and evaluation
+    of truth conditions."""
+
+    DEFAULT_EXAMPLE_SETTINGS = {
+        'N' : 3,
+        'contingent' : False,
+        'disjoint' : False,
+        'max_time' : 1,
+    }
+
+    def __init__(self, N):
+
+        # Initialize the superclass to set defaults
+        super().__init__(N)
+
+        ### Define the Z3 primitives ###
+
+        WorldStateSort = z3.BitVecSort(N)
+        # Create a sort for world states using bitvectors such as <001101>
+        # N determines how many distinct worlds we can represent
+
+        self.task = z3.Function(
+            "Task",
+            WorldStateSort,
+            WorldStateSort,
+            z3.BoolSort()
+        )  # Define a binary task relation between world states
+        
+        TimeSort = z3.IntSort()
+        # Create a sort for times using integers
+
+        self.main_time = z3.Int('main_time', TimeSort)
+
+        self.truth_condition = z3.Function(
+            "truth_condition",
+            WorldStateSort,
+            z3.BoolSort()
+        )
+
+        self.main_world = z3.Array('main_world', TimeSort, WorldStateSort)
+        # Define an arbitrary world at which to evaluate sentences 
+        # The identity of this world may change between models
+        # For each time t, main_world[t] gives exactly one world state
+        # Use `Store(array, index, value)` to update mappings
+            # Set the world state at time 1
+            # new_main_world = z3.Store(self.main_world, 1, some_world_state)
+        # Use `Select(array, index)` to get values
+            # Get the world state at time 0
+            # world_at_time_0 = z3.Select(self.main_world, 0)
+
+        ### Define the frame constraints ###
+
+        tau = z3.Array('frame_world_tau', TimeSort, WorldStateSort)
+        # declare possible world variable named to reflect the constraint
+        x, y = z3.Ints("frame_time_x frame_time_y")
+        # declare two time variables with names to reflect the constraint
+        lawful = z3.ForAll(
+            tau,
+            z3.ForAll(
+                [x, y],
+                z3.Implies(
+                    y == x + 1,
+                    self.task(tau[x], tau[y])
+                )
+            )
+        )  # Require there to be a task from every world state to its successor
+
+        u, v = z3.BitVecs("frame_state_u frame_state_v", N)
+        # Define variables for world states with names to indicate constraint
+        seriel = ForAll(
+            u,
+            Exists(
+                v,
+                self.task(u, v)
+            )
+        )  # Serial: Require every world state to see some state
+
+        # Define frame constraints
+        self.frame_constraints = [
+            lawful,
+            seriel,
+        ]
+
+        # Define invalidity conditions
+        self.premise_behavior = self.true_at
+        self.conclusion_behavior = self.false_at
+
+
+    def true_at(self, sentence, eval_world, eval_time):
+        """Returns a Z3 formula that is satisfied when the sentence is true at eval_world at eval_time.
+
+        Args:
+            sentence: The sentence to evaluate
+            eval_world: The world state array at which to evaluate the sentence
+            eval_time: The time point at which to evaluate the sentence
+            
+        Returns:
+            Z3 formula that is satisfied when sentence is true at eval_world at eval_time
+        """
+        sentence_letter = sentence.sentence_letter  # store sentence letter
+
+        # base case
+        if sentence_letter is not None:                     
+            eval_world_state = eval_world[eval_time]
+            return self.truth_condition(eval_world_state)  # true in main_world
+
+        # recursive case
+        operator = sentence.operator  # store operator
+        arguments = sentence.arguments or () # store arguments
+        return operator.true_at(
+            *arguments,  # this is a possibly empty tuple
+            eval_world,  # passed through from above
+            eval_time  # passed through from above
+        )  # apply semantics for the operator
+
+    def false_at(self, sentence, eval_world, eval_time):
+        """Returns a Z3 formula that is satisfied when the sentence is false at eval_world at eval_time.
+
+        Args:
+            sentence: The sentence to evaluate
+            eval_world: The world state array at which to evaluate the sentence
+            eval_time: The time point at which to evaluate the sentence
+            
+        Returns:
+            Z3 formula that is satisfied when sentence is false at eval_world at eval_time
+        """
+        return z3.Not(self.true_at(sentence, eval_world, eval_time))
+
+
+class IntensionalProposition(IntensionalPropositionDefaults):
+    """Defines the proposition assigned to the sentences of the language.
+    all user has to keep for their own class is super().__init__ and super().__poster_init__
+    in the __init__ method.
+    """
+
+    def __init__(self, sentence, model_structure, eval_world='main', eval_time='now'):
+
+        super().__init__(sentence, model_structure)
+        self.eval_world = model_structure.main_world if eval_world == 'main' else eval_world
+        self.eval_time = model_structure.main_time if eval_time == 'now' else eval_time
+        self.truth_set, self.false_set = self.find_proposition()
+        
+
+    def __eq__(self, other):
+        return (
+            self.truth_set == other.truth_condition
+            and self.false_set == other.false_condition
+            and self.name == other.name
+        )
+
+    def proposition_constraints(self, sentence_letter):
+        """Returns Z3 constraints for a sentence letter based on user settings.
+        
+        Generates classical constraints and optional constraints (non-null, contingent, 
+        and disjoint) depending on the user settings.
+        
+        Args:
+            sentence_letter: The sentence letter to generate constraints for
+        
+        Returns:
+            list: Z3 constraints for the sentence letter
+        """
+        semantics = self.semantics
+
+        def get_contingent_constraints():
+            """The contingent constraints require that a sentence letter is true
+            at some world state and false at some world state."""
+            x, y = z3.BitVecs("prop_cont_x prop_cont_y", semantics.N)
+            possibly_true = Exists(
+                x,
+                semantics.true_at(x, sentence_letter)
+            )
+            possibly_false = Exists(
+                y,
+                semantics.false_at(x, sentence_letter)
+            )
+            return [possibly_true, possibly_false]
+
+        def get_disjoint_constraints():
+            """The disjoint_constraints ensure that no two sentence letters can
+            be true at the same world state."""
+            x = z3.BitVec("prop_dj_x", semantics.N)
+            disjoint_constraints = []
+            for other_letter in self.sentence_letters:
+                if other_letter is not sentence_letter:
+                    other_disjoint_atom = ForAll(
+                        x,
+                        z3.Or(
+                            semantics.false_at(x, sentence_letter),
+                            semantics.false_at(x, other_letter)
+                        )
+                    )
+                    disjoint_constraints.append(other_disjoint_atom)
+            return disjoint_constraints
+
+        # Collect constraints
+        constraints = []
+        if self.settings['contingent']:
+            constraints.extend(get_contingent_constraints())
+        if self.settings['disjoint']:
+            constraints.extend(get_disjoint_constraints())
+        return constraints
+
+    def find_proposition(self):
+        """takes self, returns the V, F tuple
+        used in find_verifiers_and_falsifiers"""
+        all_world_states = self.model_structure.all_bits
+        model = self.model_structure.z3_model
+        semantics = self.semantics
+        eval_world = self.eval_world
+        eval_time = self.eval_time
+        operator = self.operator
+        arguments = self.arguments or ()
+        sentence_letter = self.sentence_letter
+        if sentence_letter is not None:
+            T, F = set(), set()
+            for world_state in all_world_states:
+                T.add(world_state) if model.evaluate(
+                    semantics.true_at(world_state, sentence_letter)
+                ) else F.add(world_state)
+            return T, F
+        if operator is not None:
+            return operator.find_truth_condition(*arguments, eval_world, eval_time)
+        raise ValueError(f"Their is no proposition for {self}.")
+
+    def print_proposition(self, eval_world, eval_time, indent_num, use_colors):
+        semantics = self.model_structure.model_constraints.semantics
+        eval_world_state = eval_world[eval_time]
+        truth_value = semantics.true_at(eval_world_state)
+        world_state = bitvec_to_substates(eval_world_state, semantics.N)
+        RESET, FULL, PART = self.set_colors(
+            self.name,
+            indent_num,
+            truth_value,
+            world_state,
+            use_colors
+        )
+        print(
+            f"{'  ' * indent_num}{FULL}|{self.name}| = {self}{RESET}"
+            f"  {PART}({truth_value} in {world_state}){RESET}"
+        )
