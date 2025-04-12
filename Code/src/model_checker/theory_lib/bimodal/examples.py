3e489d3d9639321b72808484cbf8448d7c70409a
benbrastmckie <benbrastmckie@gmail.com>
2025-04-11 21:29:18 -0400

    cleanup bimodal examples

--- Code/src/model_checker/theory_lib/bimodal/examples.py
+++ Code/src/model_checker/theory_lib/bimodal/examples.py
@@ -55,6 +55,11 @@ bimodal_theory = {
 }
 
 
+
+##############################################################################
+############################### COUNTERMODELS ################################
+##############################################################################
+
 #################################
 ### EXTENSIONAL COUNTERMODELS ###
 #################################
@@ -116,9 +121,9 @@ MD_CM_2_example = [
     MD_CM_2_settings,
 ]
 
-# MD_CM_3: 
-MD_CM_3_premises = ['A', '\\neg B']
-MD_CM_3_conclusions = ['\\Diamond (A \\wedge B)']
+# MD_CM_3: ACTUALITY TO NECESSITY
+MD_CM_3_premises = ['A']
+MD_CM_3_conclusions = ['\\Box A']
 MD_CM_3_settings = {
     'N' : 1,
     'M' : 1,
@@ -133,9 +138,9 @@ MD_CM_3_example = [
     MD_CM_3_settings,
 ]
 
-# MD_CM_4: 
-MD_CM_4_premises = ['A']
-MD_CM_4_conclusions = ['\\Box A']
+# MD_CM_4: POSSIBILITY TO ACTUALITY
+MD_CM_4_premises = ['\\Diamond A']
+MD_CM_4_conclusions = ['A']
 MD_CM_4_settings = {
     'N' : 1,
     'M' : 1,
@@ -150,10 +155,9 @@ MD_CM_4_example = [
     MD_CM_4_settings,
 ]
 
-
-# MD_CM_5: 
-MD_CM_5_premises = ['\\Diamond A', '\\Diamond B']
-MD_CM_5_conclusions = ['\\Diamond (A \\wedge B)']
+# MD_CM_5: POSSIBILITY TO NECESSITY
+MD_CM_5_premises = ['\\Diamond A']
+MD_CM_5_conclusions = ['\\Box A']
 MD_CM_5_settings = {
     'N' : 1,
     'M' : 1,
@@ -168,10 +172,9 @@ MD_CM_5_example = [
     MD_CM_5_settings,
 ]
 
-
-# MD_CM_6: 
-MD_CM_6_premises = ['\\Diamond A']
-MD_CM_6_conclusions = ['\\Box A']
+# MD_CM_6: INCOMPATIBLE POSSIBILITIES
+MD_CM_6_premises = ['\\Diamond A', '\\Diamond B']
+MD_CM_6_conclusions = ['\\Diamond (A \\wedge B)']
 MD_CM_6_settings = {
     'N' : 1,
     'M' : 1,
@@ -184,24 +187,6 @@ MD_CM_6_example = [
     MD_CM_6_premises,
     MD_CM_6_conclusions,
     MD_CM_6_settings,
-]
-
-
-# MD_CM_7: 
-MD_CM_7_premises = ['\\Diamond A', '\\Diamond B']
-MD_CM_7_conclusions = ['(A \\wedge \\neg B)']
-MD_CM_7_settings = {
-    'N' : 1,
-    'M' : 1,
-    'contingent' : False,
-    'disjoint' : False,
-    'max_time' : 2,
-    'expectation' : True,
-}
-MD_CM_7_example = [
-    MD_CM_7_premises,
-    MD_CM_7_conclusions,
-    MD_CM_7_settings,
 ]
 
 
@@ -251,7 +236,7 @@ TN_CM_2_example = [
 ### BIMODAL COUNTERMODELS ###
 #############################
 
-# TN_CM_1: 
+# TN_CM_1: ALL FUTURE TO NECESSITY
 BM_CM_1_premises = ['\\Future A']
 BM_CM_1_conclusions = ['\\Box A']
 BM_CM_1_settings = {
@@ -268,7 +253,7 @@ BM_CM_1_example = [
     BM_CM_1_settings,
 ]
 
-# TN_CM_2: 
+# TN_CM_2: ALL PAST TO NECESSITY
 BM_CM_2_premises = ['\\Past A']
 BM_CM_2_conclusions = ['\\Box A']
 BM_CM_2_settings = {
@@ -285,7 +270,112 @@ BM_CM_2_example = [
     BM_CM_2_settings,
 ]
 
+# MD_CM_3: POSSIBILITY TO SOME FUTURE
+BM_CM_3_premises = ['\\Diamond A']
+BM_CM_3_conclusions = ['\\future']
+BM_CM_3_settings = {
+    'N' : 2,
+    'M' : 2,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 2,
+    'expectation' : True,
+}
+BM_CM_3_example = [
+    BM_CM_3_premises,
+    BM_CM_3_conclusions,
+    BM_CM_3_settings,
+]
 
+# MD_CM_4: POSSIBILITY TO SOME PAST
+BM_CM_4_premises = ['\\Diamond A']
+BM_CM_4_conclusions = ['\\past A']
+BM_CM_4_settings = {
+    'N' : 2,
+    'M' : 2,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 2,
+    'expectation' : True,
+}
+BM_CM_4_example = [
+    BM_CM_4_premises,
+    BM_CM_4_conclusions,
+    BM_CM_4_settings,
+]
+
+
+
+##############################################################################
+################################## THEOREMS ##################################
+##############################################################################
+
+############################
+### EXTENSIONAL THEOREMS ###
+############################
+
+# EX_TH_1: CONJUNCTION TO DISJUNCTION 
+EX_TH_1_premises = ['(A \\wedge B)']
+EX_TH_1_conclusions = ['(A \\vee B)']
+EX_TH_1_settings = {
+    'N' : 1,
+    'M' : 1,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 2,
+    'expectation' : True,
+}
+EX_TH_1_example = [
+    EX_TH_1_premises,
+    EX_TH_1_conclusions,
+    EX_TH_1_settings,
+]
+
+
+
+######################
+### MODAL THEOREMS ###
+######################
+
+# MD_TH_1: NECESSITY DISTRIBUTE OVER IMPLICATION
+MD_TH_1_premises = ['\\Box (A \\rightarrow B)']
+MD_TH_1_conclusions = ['(\\Box A \\rightarrow \\Box B)']
+MD_TH_1_settings = {
+    'N' : 2,
+    'M' : 2,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 2,
+    'expectation' : False,
+}
+MD_TH_1_example = [
+    MD_TH_1_premises,
+    MD_TH_1_conclusions,
+    MD_TH_1_settings,
+]
+
+
+
+######################
+### TENSE THEOREMS ###
+######################
+
+# MD_TH_2: 
+TN_TH_2_premises = ['A']
+TN_TH_2_conclusions = ['\\Future \\past A']
+TN_TH_2_settings = {
+    'N' : 2,
+    'M' : 2,
+    'contingent' : False,
+    'disjoint' : False,
+    'max_time' : 2,
+    'expectation' : False,
+}
+TN_TH_2_example = [
+    TN_TH_2_premises,
+    TN_TH_2_conclusions,
+    TN_TH_2_settings,
+]
 
 
 
@@ -293,10 +383,9 @@ BM_CM_2_example = [
 ### BIMODAL THEOREMS ###
 ########################
 
-# BM_TH_1: Necessary Future Perpetuity
+# BM_TH_1: NECESSITY TO ALL FUTURE (PERPETUITY)
 BM_TH_1_premises = ['\\Box A']
 BM_TH_1_conclusions = ['\\Future A']
-# BM_TH_1_conclusions = ['\\Past A', 'A', '\\Future A']
 BM_TH_1_settings = {
     'N' : 1,
     'M' : 2,
@@ -311,15 +400,15 @@ BM_TH_1_example = [
     BM_TH_1_settings,
 ]
 
-# BM_TH_2: Possible Future Perpetuity
-BM_TH_2_premises = ['\\future A']
-BM_TH_2_conclusions = ['\\Diamond A']
+# BM_TH_2: NECESSITY TO ALL PAST (PERPETUITY)
+BM_TH_2_premises = ['\\Box A']
+BM_TH_2_conclusions = ['\\Past A']
 BM_TH_2_settings = {
     'N' : 1,
     'M' : 2,
     'contingent' : False,
     'disjoint' : False,
-    'max_time' : 5,
+    'max_time' : 2,
     'expectation' : False,
 }
 BM_TH_2_example = [
@@ -328,15 +417,15 @@ BM_TH_2_example = [
     BM_TH_2_settings,
 ]
 
-# BM_TH_3: Necessary Past Perpetuity
-BM_TH_3_premises = ['\\Box A']
-BM_TH_3_conclusions = ['\\Past A']
+# BM_TH_3: POSSIBILITY TO SOME FUTURE (PERPETUITY)
+BM_TH_3_premises = ['\\future A']
+BM_TH_3_conclusions = ['\\Diamond A']
 BM_TH_3_settings = {
     'N' : 1,
     'M' : 2,
     'contingent' : False,
     'disjoint' : False,
-    'max_time' : 2,
+    'max_time' : 5,
     'expectation' : False,
 }
 BM_TH_3_example = [
@@ -345,7 +434,7 @@ BM_TH_3_example = [
     BM_TH_3_settings,
 ]
 
-# BM_TH_4: Possible Past Perpetuity 
+# BM_TH_4: POSSIBILITY TO SOME PAST (PERPETUITY) 
 BM_TH_4_premises = ['\\past A']
 BM_TH_4_conclusions = ['\\Diamond A']
 BM_TH_4_settings = {
@@ -364,46 +453,6 @@ BM_TH_4_example = [
 
 
 
-
-
-
-######################
-### MODAL THEOREMS ###
-######################
-
-# MD_TH_1:
-MD_TH_1_premises = ['\\Box (A \\rightarrow B)']
-MD_TH_1_conclusions = ['(\\Box A \\rightarrow \\Box B)']
-MD_TH_1_settings = {
-    'N' : 2,
-    'M' : 2,
-    'contingent' : False,
-    'disjoint' : False,
-    'max_time' : 2,
-    'expectation' : False,
-}
-MD_TH_1_example = [
-    MD_TH_1_premises,
-    MD_TH_1_conclusions,
-    MD_TH_1_settings,  # can use example_settings from above
-]
-
-# MD_TH_2:
-MD_TH_2_premises = ['A']
-MD_TH_2_conclusions = ['\\Future \\past A']
-MD_TH_2_settings = {
-    'N' : 2,
-    'M' : 2,
-    'contingent' : False,
-    'disjoint' : False,
-    'max_time' : 2,
-    'expectation' : False,
-}
-MD_TH_2_example = [
-    MD_TH_2_premises,
-    MD_TH_2_conclusions,
-    MD_TH_2_settings,
-]
 
 
 
@@ -430,7 +479,6 @@ test_example_range = {
     "MD_CM_4" : MD_CM_4_example,
     "MD_CM_5" : MD_CM_5_example,
     "MD_CM_6" : MD_CM_6_example,
-    "MD_CM_7" : MD_CM_7_example,
 
     # Tense Countermodels
     "TN_CM_1" : TN_CM_1_example,
@@ -439,18 +487,25 @@ test_example_range = {
     # Bimodal Countermodels
     "BM_CM_1" : BM_CM_1_example,
     "BM_CM_2" : BM_CM_2_example,
+    "BM_CM_3" : BM_CM_3_example,
+    "BM_CM_4" : BM_CM_4_example,
 
     ### THEOREMS ###
+
+    # Extensional Theorems
+    "EX_TH_1" : EX_TH_1_example,
+
+    # Modal Theorems
+    "MD_TH_1" : MD_TH_1_example,
+
+    # Tense Theorems
+    "TN_TH_2" : TN_TH_2_example,
 
     # Bimodal Theorems
     "BM_TH_1" : BM_TH_1_example,
     "BM_TH_2" : BM_TH_2_example,
     "BM_TH_3" : BM_TH_3_example,
     "BM_TH_4" : BM_TH_4_example,
-
-    # Modal Theorems
-    "MD_TH_1" : MD_TH_1_example,
-    "MD_TH_2" : MD_TH_2_example,
 }
 
 # NOTE: at least one example is required, multiple are permitted for comparison
@@ -462,31 +517,37 @@ example_range = {
     "EX_CM_1" : EX_CM_1_example,
     
     # Modal Countermodels
-    # "MD_CM_1" : MD_CM_1_example,
-    # "MD_CM_2" : MD_CM_2_example,
-    # "MD_CM_3" : MD_CM_3_example,
-    # "MD_CM_4" : MD_CM_4_example,
-    # "MD_CM_5" : MD_CM_5_example,
-    # "MD_CM_6" : MD_CM_6_example,
-    # "MD_CM_7" : MD_CM_7_example,
+    "MD_CM_1" : MD_CM_1_example,
+    "MD_CM_2" : MD_CM_2_example,
+    "MD_CM_3" : MD_CM_3_example,
+    "MD_CM_4" : MD_CM_4_example,
+    "MD_CM_5" : MD_CM_5_example,
+    "MD_CM_6" : MD_CM_6_example,
 
     # Tense Countermodels
-    # "TN_CM_1" : TN_CM_1_example,
-    # "TN_CM_2" : TN_CM_2_example,
+    "TN_CM_1" : TN_CM_1_example,
+    "TN_CM_2" : TN_CM_2_example,
 
     # Bimodal Countermodels
-    # "BM_CM_1" : BM_CM_1_example,
-    # "BM_CM_2" : BM_CM_2_example,
+    "BM_CM_1" : BM_CM_1_example,
+    "BM_CM_2" : BM_CM_2_example,
+    "BM_CM_3" : BM_CM_3_example,
+    "BM_CM_4" : BM_CM_4_example,
 
     ### THEOREMS ###
 
-    # Bimodal Theorems
-    # "BM_TH_1" : BM_TH_1_example,
-    # "BM_TH_2" : BM_TH_2_example,
-    # "BM_TH_3" : BM_TH_3_example,
-    # "BM_TH_4" : BM_TH_4_example,
+    # Extensional Theorems
+    "EX_TH_1" : EX_TH_1_example,
 
     # Modal Theorems
-    # "MD_TH_1" : MD_TH_1_example,
-    # "MD_TH_2" : MD_TH_2_example,
+    "MD_TH_1" : MD_TH_1_example,
+
+    # Tense Theorems
+    "TN_TH_2" : TN_TH_2_example,
+
+    # Bimodal Theorems
+    "BM_TH_1" : BM_TH_1_example,
+    "BM_TH_2" : BM_TH_2_example,
+    "BM_TH_3" : BM_TH_3_example,
+    "BM_TH_4" : BM_TH_4_example,
 }
