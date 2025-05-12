aa4da2b75bfd74875a717221a8e54b9fd0f4132c
mfbuit46 <mfbuit46@mit.edu>
2025-05-12 10:47:02 -0400

    began files on redoing Champollion

--- Code/src/model_checker/theory_lib/exclusion/examples.py
+++ Code/src/model_checker/theory_lib/exclusion/examples.py
@@ -920,6 +920,27 @@ EX_CM_14_example = [
     EX_CM_14_settings
 ]
 
+# DEMORGANS IDENTITY
+EX_DMI_premises = []
+EX_DMI_conclusions = ["(\\exclude (P \\uniwedge Q) \\uniequiv (\\exclude P \\univee \\exclude Q))"] # should find cm if here
+EX_DMI_settings = {
+    'N' : 3,
+    'possible' : False,
+    'contingent' : False,
+    'non_empty' : False,
+    'non_null' : False,
+    'disjoint' : False,
+    'fusion_closure' : False,
+    'max_time' : 1,
+    'expectation' : False,
+}
+EX_DMI_example = [
+    EX_DMI_premises,
+    EX_DMI_conclusions,
+    EX_DMI_settings
+]
+
+
 
 
 ###############################################
@@ -976,19 +997,19 @@ test_example_range = {
 example_range = {
     # Countermodels
     # "EX_CM_0" : EX_CM_0_example,
-    # "EX_CM_1" : EX_CM_1_example,
+    "EX_CM_1" : EX_CM_1_example,
     # "EX_CM_5" : EX_CM_5_example,
     # "EX_CM_8" : EX_CM_8_example,
-    "EX_CM_9" : EX_CM_9_example,
+    # "EX_CM_9" : EX_CM_9_example,
     # "EX_CM_10" : EX_CM_10_example,
     # "EX_CM_11" : EX_CM_11_example,
     # "EX_CM_12" : EX_CM_12_example,
     # "EX_CM_13" : EX_CM_13_example,
-    "EX_CM_15" : EX_CM_15_example,
+    # "EX_CM_15" : EX_CM_15_example,
 
     # Theorems
     # "EX_TH_1" : EX_TH_1_example,
-    # "EX_TH_2" : EX_TH_2_example, 
+    "EX_TH_2" : EX_TH_2_example, 
     # "EX_TH_3" : EX_TH_3_example,
     # "EX_TH_4" : EX_TH_4_example,
     # "EX_TH_5" : EX_TH_5_example,
@@ -1003,14 +1024,17 @@ example_range = {
     # "EX_TH_14" : EX_TH_14_example,
     # "EX_TH_15" : EX_TH_15_example,
     # "EX_TH_16" : EX_TH_16_example,
-    "EX_TH_17" : EX_TH_17_example,
-    "EX_TH_18" : EX_TH_18_example,
+    # "EX_TH_17" : EX_TH_17_example,
+    # "EX_TH_18" : EX_TH_18_example,
     # "EX_CM_2" : EX_CM_2_example,
     # "EX_CM_3" : EX_CM_3_example,
     # "EX_CM_4" : EX_CM_4_example,
     # "EX_CM_6" : EX_CM_6_example,
     # "EX_CM_7" : EX_CM_7_example,
     # "EX_CM_14" : EX_CM_14_example,
+
+    # Other
+    "EX_DMI": EX_DMI_example,
 }
 
 
---
+++ Code/src/model_checker/theory_lib/exclusion/operators_new.py
@@ -0,0 +1,288 @@
+##########################
+### DEFINE THE IMPORTS ###
+##########################
+
+import z3
+
+from model_checker.utils import (
+    ForAll,
+    Exists,
+)
+from model_checker import syntactic
+
+class ExclusionOperator(syntactic.Operator):
+    """doc string place holder"""
+
+    name = "\\exclude"
+    arity = 1
+
+    def true_at(self, arg, eval_point):
+        """doc string place holder"""
+        x = z3.BitVec(f"ver \\exclude {arg}", self.semantics.N) # think this has to be a unique name
+        return Exists(
+            x,
+            z3.And(
+                self.extended_verify(x, arg, eval_point),
+                self.semantics.is_part_of(x, eval_point)
+            )
+        )
+
+    def extended_verify(self, state, argument, eval_point):
+        """Defines the extended verification conditions for the exclusion operator.
+        
+        This function implements the formal semantics of exclusion by defining when a state
+        is an extended verifier of an exclusion formula. A state is an extended verifier if:
+        
+        1. For every extended verifier v of the argument, there exists a part s of v that is 
+           excluded by h(v), where h is a function mapping verifiers to states
+        2. For all extended verifiers x of the argument, h(x) is a part of the given state
+        3. The given state is minimal with respect to condition 2
+        
+        Args:
+            state: A bitvector representing the state to check
+            argument: The argument of the exclusion operator
+            eval_point: The evaluation point in the model
+            
+        Returns:
+            A Z3 formula that is true iff the state is an extended verifier of the exclusion
+            formula at the given evaluation point
+        """
+
+        # Abbreviations
+        semantics = self.semantics
+        N = semantics.N
+        extended_verify = semantics.extended_verify
+        excludes = semantics.excludes
+        is_part_of = semantics.is_part_of
+
+        # # NOTE: that Z3 can introduce arbitrary functions demonstrates its expressive power
+        # h = z3.Function(
+        #     f"h_{state, argument}",   # function name
+        #     z3.BitVecSort(N),           # bitvector argument type
+        #     z3.BitVecSort(N)            # bitvector return type
+        # )
+
+        h = z3.Array(
+            f"h_{state, argument}",   # function name
+            z3.BitVecSort(N),           # bitvector argument type
+            z3.BitVecSort(N)            # bitvector return type
+        )
+
+
+
+        x, y, z, u, v = z3.BitVecs("x y z u v", N)
+
+        # state_UB = ForAll( # 2. h(z) is a part of the state for all extended_verifiers z of the argument
+        #         z,
+        #         z3.Implies(
+        #             extended_verify(z, argument, eval_point),
+        #             is_part_of(h(z), state),
+        #         )
+        #     )
+        # state_LUB = z3.Not(Exists(y, z3.And(ForAll(z, z3.Implies(extended_verify(x, argument, eval_point), is_part_of(h(z),y))), z3.And(is_part_of(y,state), y != state))))
+        # exclude_part = ForAll( # 1. for every extended_verifiers x of the argument, there 
+        #         x,  #    is some part y of x where h(x) excludes y                                    
+        #         z3.Implies(
+        #             extended_verify(x, argument, eval_point), # member of argument's set of verifiers
+        #             Exists(
+        #                 y,
+        #                 z3.And(
+        #                     is_part_of(y, x),
+        #                     excludes(h(x), y)
+        #                 )
+        #             )
+        #         )
+        #     )
+        return z3.Exists(h, z3.And(ForAll( # 2. h(z) is a part of the state for all extended_verifiers z of the argument
+                z,
+                z3.Implies(
+                    extended_verify(z, argument, eval_point),
+                    is_part_of(h[z], state),
+                )
+            ), 
+                      z3.Not(Exists(y, z3.And(ForAll(z, z3.Implies(extended_verify(x, argument, eval_point), is_part_of(h[z],y))), z3.And(is_part_of(y,state), y != state)))), 
+                      ForAll( # 1. for every extended_verifiers x of the argument, there 
+                x,  #    is some part y of x where h(x) excludes y                                    
+                z3.Implies(
+                    extended_verify(x, argument, eval_point), # member of argument's set of verifiers
+                    Exists(
+                        y,
+                        z3.And(
+                            is_part_of(y, x),
+                            excludes(h[x], y)
+                        )
+                    )
+                )
+            ))
+        )
+
+
+
+
+        # return z3.And(
+        #     ForAll( # 1. for every extended_verifiers x of the argument, there 
+        #         x,  #    is some part y of x where h(x) excludes y                                    
+        #         z3.Implies(
+        #             extended_verify(x, argument, eval_point), # member of argument's set of verifiers
+        #             Exists(
+        #                 y,
+        #                 z3.And(
+        #                     is_part_of(y, x),
+        #                     excludes(h(x), y)
+        #                 )
+        #             )
+        #         )
+        #     ),
+        #     ForAll( # 2. h(z) is a part of the state for all extended_verifiers z of the argument
+        #         z,
+        #         z3.Implies(
+        #             extended_verify(z, argument, eval_point),
+        #             is_part_of(h(z), state),
+        #         )
+        #     ),
+        #     ForAll( # 3. the state is the smallest state to satisfy condition 2
+        #         u,
+        #         z3.Implies(
+        #             ForAll(
+        #                 v,
+        #                 z3.Implies(
+        #                     extended_verify(v, argument, eval_point),
+        #                     is_part_of(h(v), state)
+        #                 )
+        #             ),
+        #             is_part_of(state, u)
+        #         )
+        #     )
+        # )
+
+    def find_verifiers(self, argument, eval_point):
+        """Returns the set of precluders for the argument's proposition."""
+        return argument.proposition.precluders
+
+    def print_method(self, sentence_obj, eval_point, indent_num, use_colors):
+        """Prints the proposition for sentence_obj, increases the indentation
+        by 1, and prints the argument."""
+        self.general_print(sentence_obj, eval_point, indent_num, use_colors)
+
+
+class UniAndOperator(syntactic.Operator):
+    """doc string place holder"""
+
+    name = "\\uniwedge"
+    arity = 2
+
+    def true_at(self, leftarg, rightarg, eval_point):
+        """doc string place holder
+        args are derived_objects I think, def original_type or derived_object
+        (ie of second or third kind)
+        """
+        sem = self.semantics
+        return z3.And(
+            sem.true_at(leftarg, eval_point),
+            sem.true_at(rightarg, eval_point)
+        )
+
+    def extended_verify(self, state, leftarg, rightarg, eval_point):
+        x = z3.BitVec("ex_ver_x", self.semantics.N)
+        y = z3.BitVec("ex_ver_y", self.semantics.N)
+        return Exists(
+            [x, y],
+            z3.And(
+                self.semantics.fusion(x, y) == state,
+                self.semantics.extended_verify(x, leftarg, eval_point),
+                self.semantics.extended_verify(y, rightarg, eval_point),
+            ),
+        )
+
+    def find_verifiers(self, left_sent_obj, right_sent_obj, eval_point):
+        """Takes sentences objects as arguments, finds their verifiers and
+        falsifiers, and returns the verifiers and falsifiers for the operator"""
+        Y_V = left_sent_obj.proposition.find_proposition()
+        Z_V = right_sent_obj.proposition.find_proposition()
+        return self.semantics.product(Y_V, Z_V)
+
+    def print_method(self, sentence_obj, eval_point, indent_num, use_colors):
+        """Prints the proposition for sentence_obj, increases the indentation
+        by 1, and prints both of the arguments."""
+        self.general_print(sentence_obj, eval_point, indent_num, use_colors)
+
+
+class UniOrOperator(syntactic.Operator):
+    """doc string place holder"""
+
+    name = "\\univee"
+    arity = 2
+
+    def true_at(self, leftarg, rightarg, eval_point):
+        """doc string place holder"""
+        sem = self.semantics
+        return z3.Or(
+            sem.true_at(leftarg, eval_point), sem.true_at(rightarg, eval_point)
+        )
+
+    def extended_verify(self, state, leftarg, rightarg, eval_point):
+        return z3.Or(
+            self.semantics.extended_verify(state, leftarg, eval_point),
+            self.semantics.extended_verify(state, rightarg, eval_point),
+            # same as bilateral except no And in def
+        )
+
+    def find_verifiers(self, left_sent_obj, right_sent_obj, eval_point):
+        Y_V = left_sent_obj.proposition.find_proposition()
+        Z_V = right_sent_obj.proposition.find_proposition()
+        return Y_V.union(Z_V)
+
+    def print_method(self, sentence_obj, eval_point, indent_num, use_colors):
+        """Prints the proposition for sentence_obj, increases the indentation
+        by 1, and prints both of the arguments."""
+        self.general_print(sentence_obj, eval_point, indent_num, use_colors)
+
+class UniIdentityOperator(syntactic.Operator):
+    """doc string place holder"""
+
+    name = "\\uniequiv"
+    arity = 2
+
+    def true_at(self, leftarg, rightarg, eval_point):
+        """doc string place holder"""
+        N = self.semantics.N
+        sem = self.semantics
+        x = z3.BitVec("t_id_x", N)
+        return z3.And(
+            ForAll(
+                x,
+                z3.Implies(
+                    sem.extended_verify(x, leftarg, eval_point),
+                    sem.extended_verify(x, rightarg, eval_point)
+                ),
+            ),
+            ForAll(
+                x,
+                z3.Implies(
+                    sem.extended_verify(x, rightarg, eval_point),
+                    sem.extended_verify(x, leftarg, eval_point)
+                ),
+            ),
+        )
+
+    def extended_verify(self, state, leftarg, rightarg, eval_point):
+        return z3.And(
+            state == self.semantics.null_state,
+            self.true_at(leftarg, rightarg, eval_point)
+        )
+
+    def find_verifiers(self, left_sent_obj, right_sent_obj, eval_point):
+        Y_V = left_sent_obj.proposition.find_proposition()
+        Z_V = right_sent_obj.proposition.find_proposition()
+        return {self.semantics.null_state} if Y_V == Z_V else set()
+    
+    def print_method(self, sentence_obj, eval_point, indent_num, use_colors):
+        """Prints the proposition for sentence_obj, increases the indentation
+        by 1, and prints both of the arguments."""
+        self.general_print(sentence_obj, eval_point, indent_num, use_colors)
+
+
+exclusion_operators = syntactic.OperatorCollection(
+    UniAndOperator, UniOrOperator, ExclusionOperator, # extensional
+    UniIdentityOperator, # constitutive
+)
